27203b00fa54c7cf5ee4205336320a8a7a7d202b
dontirrigateme <smokey.alley44@gmail.com>
2026-04-30 13:05:19 -0400

    Create capacitors.py

---
+++ data/capacitors.py
@@ -0,0 +1,10 @@
+QUESTIONS = [
+    {
+        "question": "What does a capacitor store?",
+        "answer": "Electrical energy"
+    },
+    {
+        "question": "What unit is capacitance measured in?",
+        "answer": "Farads"
+    }
+]
