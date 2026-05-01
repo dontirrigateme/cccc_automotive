33b8b564670993ca5347da1a62ee8bc732fb0f04
dontirrigateme <smokey.alley44@gmail.com>
2026-05-01 01:55:37 -0400

    Update fuses.py

--- data/fuses.py
+++ data/fuses.py
@@ -1,6 +1,26 @@
 QUESTIONS = [
     {
-        "question": "Test question?",
-        "answer": "Test answer."
-    }
+        "question": "A fuse protects an electrical circuit against short circuits. True or false?", #01
+        "answer": "True.",
+    },
+    {
+        "question": "A fuse is always placed far away from the power supply to protect as much of the circuit as possible.", #02
+        "answer": "False. To protect the circuit, a fuse is always placed as close as possible to the power supply.",
+    },
+    {
+        "question": "Where are accessory fuses found?", #03
+        "answer": "near the battery",
+    },
+    {
+        "question": "A fuse is the [weakest] [strongest] link in an electrical circuit.", #04
+        "answer": "weakest",
+    },
+    {
+        "question": "What happens to a circuit when a fuse blows?", #05
+        "answer": "the circuit is opened",
+    },
+    {
+        "question": "The value of a fuse is not only indicated by a number on the fuse, but also by its __________", #06
+        "answer": "color",
+    },
 ]
