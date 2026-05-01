21692bbb3b2b907c5438c0ff41c81eed5d186c7a
dontirrigateme <smokey.alley44@gmail.com>
2026-05-01 03:22:30 -0400

    Update fuses.py

--- data/fuses.py
+++ data/fuses.py
@@ -23,4 +23,24 @@ QUESTIONS = [
         "question": "The value of a fuse is not only indicated by a number on the fuse, but also by its __________", #06
         "answer": "color",
     },
+    {
+        "question": "__________ occurs if the current through a circuit exceeds a certain maximum current.", #07
+        "answer": "Overloading",
+    },
+    {
+        "question": "A fuse will always blow immediately when its limit is reached. True or false?", #08
+        "answer": "False. If the current through the fuse is not much greater than the value of the fuse, it may take a while for the fuse to blow.",
+    },
+    {
+        "question": "Overloading a wire brings great danger. The insulating properties can reduce, or the wire can even melt. This can cause a short circuit and a fire hazard. True or false?", #09
+        "answer": "True",
+    },
+    {
+        "question": "Fuses protect a motor against short circuits and overloading. True or false?", #10
+        "answer": "False. Fuses only protect a motor against short circuits, not against overloading.",
+    },
+    {
+        "question": "Fuses protect a motor against short circuits and overloading. True or false?", #11
+        "answer": "False. Fuses only protect a motor against short circuits, not against overloading.",
+    },
 ]
