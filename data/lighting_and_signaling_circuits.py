33c688d6169cc1075f68fd7a98ed5fa0bdb13cd8
dontirrigateme <smokey.alley44@gmail.com>
2026-05-01 07:41:47 -0400

    Update lighting_and_signaling_circuits.py

--- data/lighting_and_signaling_circuits.py
+++ data/lighting_and_signaling_circuits.py
@@ -1,6 +1,45 @@
 QUESTIONS = [
     {
-        "question": "A capacitor is often used to __________ and __________ interfering signals.", #01
-        "answer": "suppress and filter",
+        "question": "What kind of socket is used to connect a vehicle and a trailer?", #01
+        "answer": "a multi-pin socket",
+    },
+    {
+        "question": "All trailer plugs have a ground wire. True or false?", #02
+        "answer": "True",
+    },
+    {
+        "question": "What color is the ground wire of a trailer plug?", #03
+        "answer": "White",
+    },
+    {
+        "question": "What are the two common connector systems that are used in the US?", #04
+        "answer": "1) 4-pin flat connectors, 2) 7-pin round blade connectors",
+    },
+    {
+        "question": "What kind of plug is shown in the picture?", #05
+        "answer": "7-pin plug/socket",
+        "image": "images/7_pin_plug.png",
+    },
+    {
+        "question": "What four pins are common to all trailer feeds?", #06
+        "answer": "1) ground, 2) left turn/stop, 3) right turn/stop, 4) tail light",
+    },
+    {
+        "question": "What kind of plug is shown in the picture?", #07
+        "answer": "6-pin connector",
+        "image": "images/6_pin_plug.png",
+    },
+    {
+        "question": "What kind of plug is shown in the picture?", #08
+        "answer": "6-pin connector",
+        "image": "images/5_pin_plug",
+    },
+    {
+        "question": "For diagnosis and testing, the best place to begin testing is on the vehicle side of the plug, because that’s the active (origin) side of the circuit. True or false?", #09
+        "answer": "True",
+    },
+    {
+        "question": "What is the most common tool for assessing lighting connections on a trailer?", #10
+        "answer": "LED tester",
     },
 ]
