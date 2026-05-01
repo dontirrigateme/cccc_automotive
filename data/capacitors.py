a5dfacec1ad0628bd93cfe283696450580cd68e5
dontirrigateme <smokey.alley44@gmail.com>
2026-05-01 00:37:34 -0400

    Update capacitors.py

--- data/capacitors.py
+++ data/capacitors.py
@@ -1,10 +1,22 @@
 QUESTIONS = [
     {
-        "question": "What does a capacitor store?",
-        "answer": "Electrical energy"
+        "question": "A capacitor is often used to __________ and __________ interfering signals.",
+        "answer": "suppress and filter"
     },
     {
-        "question": "What unit is capacitance measured in?",
-        "answer": "Farads"
-    }
+        "question": "You will find capacitors in the ignition system of vehicles with __________ ignition.",
+        "answer": "distributor"
+    },
+    {
+        "question": "What is the value of a capacitor called?",
+        "answer": "capacitance"
+    },
+    {
+        "question": "What is a unit of capacitance called?",
+        "answer": "farad"
+    },
+    {
+        "question": "What is specified on the casing of a capacitor?",
+        "answer": "capacitance and voltage"
+    },
 ]
