7f6c21890a553bf01815bf38777e8c5ff54e7e29
dontirrigateme <smokey.alley44@gmail.com>
2026-05-01 02:31:08 -0400

    Update resistors.py

--- data/resistors.py
+++ data/resistors.py
@@ -1,6 +1,80 @@
 QUESTIONS = [
     {
-        "question": "Test question?",
-        "answer": "Test answer."
-    }
+        "question": "What does a resistor convert electric energy into?", #01
+        "answer": "heat",
+    },
+    {
+        "question": "Which symbol represents a variable resistor?", #02
+        "answer": "number 2",
+        "image": "images/fixed_variable_resistors.png",
+    },
+    {
+        "question": "What kind of resistor is represented by symbol #1?", #03
+        "answer": "fixed resistor",
+        "image": "images/fixed_variable_resistors.png",
+    },
+    {
+        "question": "How do you identify a resistor with a fixed value?", #04
+        "answer": "colored bands around the component",
+    },
+    {
+        "question": "How do you set a resistor with a variable value?", #05
+        "answer": "with a rotary switch",
+    },
+    {
+        "question": "Electric current can move much easier through [smaller] [larger] conductors.", #06
+        "answer": "larger",
+    },
+    {
+        "question": "Resistance increases when the area (A) is smaller. True or false?", #07
+        "answer": "True",
+    },
+    {
+        "question": "The resistance increases when the length (l) decreases. True or false?", #08
+        "answer": "False. When the length increases, the resistance increases.",
+    },
+    {
+        "question": "Current flowing more easily through some materials than others is indicated by which concept?", #09
+        "answer": "specific resistance (p)",
+    },
+    {
+        "question": "What determines the resistance value of carbon- and metal-film resistors?", #10
+        "answer": "a helix groove",
+    },
+    {
+        "question": "What kind of resistor can be used in higher-powered circuits?", #11
+        "answer": "wire-wound resistors",
+    },
+    {
+        "question": "Which resistors have soldered wires replaced by soldered pads?", #12
+        "answer": "SMD resistors",
+    },
+    {
+        "question": "Which resistors are linear and logarithmic?", #13
+        "answer": "variable resistors",
+    },
+    {
+        "question": "When a resistor is used above the maximum __________, then a breakdown can take place.", #14
+        "answer": "voltage",
+    },
+    {
+        "question": "When the __________ of a resistor is exceeded, the temperature of the resistor will become too high.", #15
+        "answer": "power",
+    },
+    {
+        "question": "How are the numeral value and tolerance of a resistor represented?", #16
+        "answer": "colored bands around the resistor",
+    },
+    {
+        "question": "On resistors with four bands, what do band 1 and 2 represent?", #17
+        "answer": "numeral value",
+    },
+    {
+        "question": "On resistors with four bands, what does band 3 represent?", #18
+        "answer": "multiplication factor",
+    },
+    {
+        "question": "On resistors with four bands, what does band 4 represent?", #19
+        "answer": "tolerance",
+    },
 ]
