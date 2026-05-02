8adbf51f8a8a2a257e17b457e87319bc266d620d
dontirrigateme <smokey.alley44@gmail.com>
2026-05-02 05:06:14 -0400

    Add questions and answers for lighting circuits

--- data/lighting_and_signaling_circuits.py
+++ data/lighting_and_signaling_circuits.py
@@ -42,4 +42,75 @@ QUESTIONS = [
         "question": "What is the most common tool for assessing lighting connections on a trailer?", #10
         "answer": "LED tester",
     },
+    {
+        "question": "A __________ is used to make the turn signals flash automatically. True or false?", #11
+        "answer": "flasher unit",
+    },
+    {
+        "question": "What does a flasher unit consist of?", #12
+        "answer": "a relay and a lse generator",
+    },
+    {
+        "question": "What happens if a lamp in a turn signal is defective?", #13
+        "answer": "it flashes faster",
+    },
+    {
+        "question": "Name the parts of the flasher unit.", #14
+        "answer": "1 = plug connection, 2 = relay, 3 = pulse generator",
+        "image": "images/flasher_unit.png",
+    },
+    {
+        "question": "What kind of lamp is used most often in headlights with low and high beams in a reflector?", #15
+        "answer": "an H4 halogen lamp",
+    },
+    {
+        "question": "Why is a black coating applied to the front of the H4 halogen low-beam lamp?", #16
+        "answer": "to prevent the light being too bright",
+        "image": "images/black_coating.png",
+    },
+    {
+        "question": "With an H4 lamp, the filament of the low beam is placed just after the focal point. True or false?", #17
+        "answer": "False. With an H4 lamp, the filament of the low beam is placed just before the focal point.",
+    },
+    {
+        "question": "The beam pattern of low-beam headlights runs upwards on one side at an angle of __________.", #18
+        "answer": "15°",
+    },
+    {
+        "question": "Conventional reflectors are often __________ reflectors.", #19
+        "answer": "parabolic",
+    },
+    {
+        "question": "Headlights are projected forward using __________.", #20
+        "answer": "reflectors",
+    },
+    {
+        "question": "What is a major disadvantage of headlights in a single parabolic reflector?", #21
+        "answer": "The low beam only uses part of the reflector. The light output is, therefore, less than with the high beam.",
+    },
+    {
+        "question": "When light falls parallel to a reflector, the reflected light comes together at one point. What is that point called?", #22
+        "answer": "the focal point",
+        "image": "images/focal_point.png",
+    },
+    {
+        "question": "What are the two types of deviating reflectors?", #23
+        "answer": "homofocal and bifocal",
+    },
+    {
+        "question": "What types of bulbs are used in headlights?", #24
+        "answer": "single-pole, double-pole, and double-filament",
+    },
+    {
+        "question": "How do you remove old UV coating, craquelure, and small scratches from a plastic headlight lens?", #25
+        "answer": "thoroughly clean the headlight, then wet sand it with 800-grit sandpaper",
+    },
+    {
+        "question": "Why can't a high-speed sander be used to polish a headlight?", #26
+        "answer": "it generates too much heat and will sand in irregularities",
+    },
+    {
+        "question": "Why are headlights sprayed with adhesive primer during refurbishing?", #27
+        "answer": "it allows the new UV coating to adhere to the polycarbonate of the headlight",
+    },
 ]
