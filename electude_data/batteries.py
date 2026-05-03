6a5804ac719949662c297ee8a957997a8fef10e3
dontirrigateme <smokey.alley44@gmail.com>
2026-05-03 04:51:16 -0400

    Add battery-related questions and answers

---
+++ electude_data/batteries.py
@@ -0,0 +1,10 @@
+QUESTIONS = [
+    {
+        "question": "Battery sensors are used to measure which parameters?", #01
+        "answer": "electrical current draw, temperature, and voltage",
+    },
+    {
+        "question": "What are the three states that determine a battery's condition?", #02
+        "answer": "state of charge, state of health, state of function",
+    },
+]
