a8f4a88fde2cfb84895d2c9e4dd87d3585a774c1
dontirrigateme <smokey.alley44@gmail.com>
2026-05-03 05:10:12 -0400

    Add questions for lubrication and cooling diagnosis

---
+++ ase_data/ch1_4_lubrication_cooling_diagnosis.py
@@ -0,0 +1,12 @@
+#01.04 Engine Repair - Lubrication & Cooling Systems Diagnosis & Repair
+
+QUESTIONS = [
+    {
+        "question": "Test question 01", #01
+        "answer": "Test answer 01",
+    },
+    {
+        "question": "Test question 02", #02
+        "answer": "Test answer 02",
+    },
+]
