6e1d0508b1b3ef79fe53bae43cb8b3ff7a8c5f89
dontirrigateme <smokey.alley44@gmail.com>
2026-05-03 05:12:12 -0400

    Add questions for hydraulic automatic transmission

---
+++ ase_data/ch2_1_hydraulic_automatic_transmission.py
@@ -0,0 +1,12 @@
+#02.01 Automatic Transmission/Transaxle - Cylinder Head & Valve Train Diagnosis & Repair
+
+QUESTIONS = [
+    {
+        "question": "Test question 01", #01
+        "answer": "Test answer 01",
+    },
+    {
+        "question": "Test question 02", #02
+        "answer": "Test answer 02",
+    },
+]
