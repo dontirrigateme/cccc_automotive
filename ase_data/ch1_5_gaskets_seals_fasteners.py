2327e542e9523a67c8316e2cea4fbeee0c79511c
dontirrigateme <smokey.alley44@gmail.com>
2026-05-03 05:11:01 -0400

    Add questions for engine repair section

---
+++ ase_data/ch1_5_gaskets_seals_fasteners.py
@@ -0,0 +1,12 @@
+#01.05 Engine Repair - Gaskets, Seals, & Fastener Service
+
+QUESTIONS = [
+    {
+        "question": "Test question 01", #01
+        "answer": "Test answer 01",
+    },
+    {
+        "question": "Test question 02", #02
+        "answer": "Test answer 02",
+    },
+]
