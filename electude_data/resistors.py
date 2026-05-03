4e6666b104fcddbf83d93007b0c2c00db25173cb
dontirrigateme <smokey.alley44@gmail.com>
2026-05-03 04:49:59 -0400

    Add resistor-related questions and answers
    Added a comprehensive set of questions and answers related to resistors, including types, functions, and applications.

---
+++ electude_data/resistors.py
@@ -0,0 +1,168 @@
+QUESTIONS = [
+    {
+        "question": "What does a resistor convert electric energy into?", #01
+        "answer": "heat",
+    },
+    {
+        "question": "Which symbol represents a variable resistor?", #02
+        "answer": "number 2",
+        "image": "images/fixed_variable_resistors.png",
+    },
+    {
+        "question": "What kind of resistor is represented by symbol #1?", #03
+        "answer": "fixed resistor",
+        "image": "images/fixed_variable_resistors.png",
+    },
+    {
+        "question": "How do you identify a resistor with a fixed value?", #04
+        "answer": "colored bands around the component",
+    },
+    {
+        "question": "How do you set a resistor with a variable value?", #05
+        "answer": "with a rotary switch",
+    },
+    {
+        "question": "Electric current can move much easier through [smaller] [larger] conductors.", #06
+        "answer": "larger",
+    },
+    {
+        "question": "Resistance increases when the area (A) is smaller. True or false?", #07
+        "answer": "True",
+    },
+    {
+        "question": "The resistance increases when the length (l) decreases. True or false?", #08
+        "answer": "False. When the length increases, the resistance increases.",
+    },
+    {
+        "question": "Current flowing more easily through some materials than others is indicated by which concept?", #09
+        "answer": "specific resistance (p)",
+    },
+    {
+        "question": "What determines the resistance value of carbon- and metal-film resistors?", #10
+        "answer": "a helix groove",
+    },
+    {
+        "question": "What kind of resistor can be used in higher-powered circuits?", #11
+        "answer": "wire-wound resistors",
+    },
+    {
+        "question": "Which resistors have soldered wires replaced by soldered pads?", #12
+        "answer": "SMD resistors",
+    },
+    {
+        "question": "Which resistors are linear and logarithmic?", #13
+        "answer": "variable resistors",
+    },
+    {
+        "question": "When a resistor is used above the maximum __________, then a breakdown can take place.", #14
+        "answer": "voltage",
+    },
+    {
+        "question": "When the __________ of a resistor is exceeded, the temperature of the resistor will become too high.", #15
+        "answer": "power",
+    },
+    {
+        "question": "How are the numeral value and tolerance of a resistor represented?", #16
+        "answer": "colored bands around the resistor",
+    },
+    {
+        "question": "On resistors with four bands, what do band 1 and 2 represent?", #17
+        "answer": "numeral value",
+    },
+    {
+        "question": "On resistors with four bands, what does band 3 represent?", #18
+        "answer": "multiplication factor",
+    },
+    {
+        "question": "On resistors with four bands, what does band 4 represent?", #19
+        "answer": "tolerance",
+    },
+    {
+        "question": "In what part of a vehicle are magnetic-dependent resistors used?", #20
+        "answer": "ABS systems",
+    },
+    {
+        "question": "The resistance of a magnetic-dependent resistor is at its maximum at an angle of __________ degrees between the direction of the current through the resistor and the magnetic field lines.", #21
+        "answer": "0",
+    },
+    {
+        "question": "The resistance of a magnetic-dependent resistor is at its minimum at an angle of __________ degrees between the direction of the current through the resistor and the magnetic field lines.", #22
+        "answer": "90",
+    },
+    {
+        "question": "What kind of resistor does a potentiometer behave like?", #23
+        "answer": "variable resistor",
+    },
+    {
+        "question": "How do you adjust the resistance on a potentiometer?", #24
+        "answer": "with a knob that fits over the shaft",
+    },
+    {
+        "question": "What kind of resistor does a potentiometer behave like?", #25
+        "answer": "variable resistor",
+    },
+    {
+        "question": "What are the parts of a potentiometer?", #26
+        "answer": "1 = connections, 2 = adjustment axis, 3 = track without variable resistor, 4 = carbion track for variable resistor, 5 = stylus, 6 = stylus carrier, 7 = housing",
+        "image": "images/potentiometer.png",
+    },
+    {
+        "question": "Potentiometers only have linear resistance. True or false?", #27
+        "answer": "False. Potentiometers can have linear or logarithmic resistance.",
+    },
+    {
+        "question": "What is represented in the symbol?", #28
+        "answer": "potentiometer",
+        "image": "images/potentiometer_symbol.png",
+    },
+    {
+        "question": "What does NTC stand for?", #29
+        "answer": "negative temperature coefficient",
+    },
+    {
+        "question": "What does PTC stand for?", #30
+        "answer": "positive temperature coefficient",
+    },
+    {
+        "question": "What does the resistance of a thermistor depend on?", #31
+        "answer": "temperature",
+    },
+    {
+        "question": "With an NTC thermistor, as temperature __________, resistance __________", #32
+        "answer": "increases, decreases",
+    },
+    {
+        "question": "With a PTC thermistor, as temperature __________, resistance __________", #33
+        "answer": "increases, increases",
+    },
+    {
+        "question": "What are the two types of PTC resistors?", #34
+        "answer": "non-linear and linear",
+    },
+    {
+        "question": "What kind of resistor is depicted?", #35
+        "answer": "NTC",
+        "image": "images/ntc.png",
+    },
+    {
+        "question": "What kind of resistor is depicted?", #36
+        "answer": "PTC",
+        "image": "images/ptc.png",
+    },
+    {
+        "question": "Non-linear PTC resistors experience a sudden, sharp increase in resistance, also called resistance jump. True or false?", #37
+        "answer": "True",
+    },
+    {
+        "question": "PTC resistors with platinum, nickel, and silicon have a (nearly) linear progression. True or false?", #38
+        "answer": "True",
+    },
+    {
+        "question": "What are some common NTC resistor applications?", #39
+        "answer": "1) coolant temperature sensor, 2) intake air temperature sensor, 3) evaporator temperature sensor, 4) ambient air temperature sensor, 5) battery temperature sensor",
+    },
+    {
+        "question": "What are some common PTC resistor applications?", #40
+        "answer": "1) fluid-level sensors, 2) air-flow sensors, 3) air-heating elements, 4) circuit protectors",
+    },
+]
