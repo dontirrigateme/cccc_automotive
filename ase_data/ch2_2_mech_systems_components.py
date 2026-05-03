0c0e20eeffb63a24363331834046f2296481f2a5
dontirrigateme <smokey.alley44@gmail.com>
2026-05-03 05:12:57 -0400

    Add questions for Automatic Transmission topic

---
+++ ase_data/ch2_2_mech_systems_components.py
@@ -0,0 +1,12 @@
+#02.02 Automatic Transmission/Transaxle - Mechanical Systems & Components
+
+QUESTIONS = [
+    {
+        "question": "Test question 01", #01
+        "answer": "Test answer 01",
+    },
+    {
+        "question": "Test question 02", #02
+        "answer": "Test answer 02",
+    },
+]
