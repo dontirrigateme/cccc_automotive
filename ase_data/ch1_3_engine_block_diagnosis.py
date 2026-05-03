d80c1f0e02e49279ba9d5fde836dc9b505870616
dontirrigateme <smokey.alley44@gmail.com>
2026-05-03 05:09:12 -0400

    Add engine block diagnosis questions to new file

---
+++ ase_data/ch1_3_engine_block_diagnosis.py
@@ -0,0 +1,12 @@
+#01.03 Engine Repair - Engine Block Diagnosis & Repair
+
+QUESTIONS = [
+    {
+        "question": "Test question 01", #01
+        "answer": "Test answer 01",
+    },
+    {
+        "question": "Test question 02", #02
+        "answer": "Test answer 02",
+    },
+]
