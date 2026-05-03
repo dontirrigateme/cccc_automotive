15a656910d240c30a87aed9fd0596b3c6e71d491
dontirrigateme <smokey.alley44@gmail.com>
2026-05-03 04:47:12 -0400

    Add capacitor-related questions and answers
    Added a list of questions and answers related to capacitors.

---
+++ electude_data/capacitors.py
@@ -0,0 +1,134 @@
+QUESTIONS = [
+    {
+        "question": "A capacitor is often used to __________ and __________ interfering signals.", #01
+        "answer": "suppress and filter",
+    },
+    {
+        "question": "You will find capacitors in the ignition system of vehicles with __________ ignition.", #02
+        "answer": "distributor",
+    },
+    {
+        "question": "What is the value of a capacitor called?", #03
+        "answer": "capacitance",
+    },
+    {
+        "question": "What is a unit of capacitance called?", #04
+        "answer": "farad",
+    },
+    {
+        "question": "What is specified on the casing of a capacitor?", #05
+        "answer": "capacitance and voltage",
+    },
+    {
+        "question": "Which number is next to the symbol of a variable capacitor?", #06
+        "answer": "number 3",
+        "image": "images/capacitor_wiring_symbols.png",
+    },
+    {
+        "question": "Number 2 is the wiring diagram symbol for what kind of capacitor?", #07
+        "answer": "polar capacitor",
+        "image": "images/capacitor_wiring_symbols.png",
+    },
+    {
+        "question": "Which one is a regular capacitor?", #08
+        "answer": "number 1",
+        "image": "images/capacitors.png",
+    },
+    {
+        "question": "Which one is a polar capacitor?", #09
+        "answer": "number 2",
+        "image": "images/capacitors.png",
+    },
+    {
+        "question": "What kind of capacitor is number 3?", #10
+        "answer": "variable capacitor",
+        "image": "images/capacitors.png",
+    },
+    {
+        "question": "A capacitor consists of two connecting leads. What are they?", #11
+        "answer": "electrodes (plates) and dielectric",
+    },
+    {
+        "question": "The dielectric is the __________ between the two electrodes.", #12
+        "answer": "insulation",
+    },
+    {
+        "question": "Which part is the connection?", #13
+        "answer": "number 1",
+        "image": "images/parts_of_a_capacitor.png",
+    },
+    {
+        "question": "Which part is the electrode/plate?", #14
+        "answer": "number 2",
+        "image": "images/parts_of_a_capacitor.png",
+    },
+    {
+        "question": "What part of a capacitor is number 3?", #15
+        "answer": "dielectric",
+        "image": "images/parts_of_a_capacitor.png",
+    },
+    {
+        "question": "What are the four commonly-used types of capacitors?", #16
+        "answer": "1) Plastic-film capacitors, 2) Ceramic capacitors, 3) Electrolytic capacitors, 4) Variable capacitor",
+    },
+    {
+        "question": "What determines the capacitance of plastic-film capacitors?", #17
+        "answer": "The width and number of turns (length)",
+    },
+    {
+        "question": "What are the parts of a plastic-film capacitor?", #18
+        "answer": "1 = electrode, 2 = dielectric",
+        "image": "images/film_capacitor_parts.png",
+    },
+    {
+        "question": "The ceramic material on a ceramic capacitor has a low resistance. True or false?", #19
+        "answer": "False. The ceramic material has a high resistance.",
+    },
+    {
+        "question": "The dielectric constant on a ceramic capacitor is high. True or false?", #20
+        "answer": "True",
+    },
+    {
+        "question": "The anode and cathode of an electrolytic capacitor consist of __________", #21
+        "answer": "aluminum foil",
+    },
+    {
+        "question": "Electrolytic capacitors are not suitable for use with alternating current. True or false?", #22
+        "answer": "True",
+    },
+    {
+        "question": "What three substances are most often used on variable capacitors?", #23
+        "answer": "mica, plastic, or air",
+    },
+    {
+        "question": "What are the parts of a ceramic capacitor?", #24
+        "answer": "1 = connection, 2 = electrode, 3 = dielectric",
+        "image": "images/ceramic_capacitor.png",
+    },
+    {
+        "question": "What are the parts of an electrolytic capacitor?", #25
+        "answer": "1 = cathode, 2 = electrolyte plates, 3 = dielectric, 4 = anode",
+        "image": "images/electrolytic_capacitor.png",
+    },
+    {
+        "question": "Identify the plates in the variable capacitor.", #26
+        "answer": "1 = fixed plate, 2 = moving plate",
+        "image": "images/variable_capacitor.png",
+    },
+    {
+        "question": "What is the resistance of a capacitor called?", #27
+        "answer": "capacitative reactance",
+    },
+    {
+        "question": "What are the two things that determine capacitative reactance?", #28
+        "answer": "capacity and frequency",
+    },
+    {
+        "question": "What kind of voltage does a capacitor block?", #29
+        "answer": "DC voltage",
+    },
+    {
+        "question": "When you suspect that a capacitor is not functioning properly, what can you do to inspect it?", #30
+        "answer": "measure the capacity with a special meter OR the resistance with a multimeter",
+    },
+]
