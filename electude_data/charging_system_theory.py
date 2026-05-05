8cbc5ba872318faf227a5209b67837a10bf2643c
dontirrigateme <smokey.alley44@gmail.com>
2026-05-05 11:20:40 -0400

    Add charging system theory questions and answers

---
+++ electude_data/charging_system_theory.py
@@ -0,0 +1,51 @@
+QUESTIONS = [
+    {
+        "question": "What is a semi-traction battery?", #001
+        "answer": "Higher capacity than the starter battery, and intended to be deeply discharged when the combustion engine is not running",
+    },
+    {
+        "question": (
+            "What are two devices that can be used to keep two batteries separate on a single vehicle?"
+        ), #002
+        "answer": (
+            "Isolating relay\n"
+            "Isolating diodes"
+        ),
+    },
+    {
+        "question": (
+            "What is an isolating relay?"
+        ), #003
+        "answer": (
+            "- An isolating relay is a simple way to separate two batteries on the same vehicle.\n"
+            "- The relay is connected to the alternator.\n"
+            "- When the alternator starts supplying power, the relay closes and connects the two batteries with the charging system.\n"
+            "- As a result, one battery can still charge the other."
+        ),
+    },
+    {
+        "question": "What is a disadvantage of an isolating relay?", #004
+        "answer": "The two batteries are connected to each other when they're meant to be kept separate",
+    },
+    {
+        "question": (
+            "What is an isolating diode?"
+        ), #005
+        "answer": (
+            "- An isolating diode is a way to separate two batteries on the same vehicle.\n"
+            "- The diodes are positioned so that the alternator will charge the batteries.\n"
+            "- The batteries are not connected to each other.\n"
+            "- Since the batteries are not connected, one battery cannot charge the other."
+        ),
+    },
+    {
+        "question": "Which number identifies the isolating relay?", #006
+        "answer": "2",
+        "image": "images/isolating_relay.png"
+    },
+    {
+        "question": "Which number identifies the isolating diode?", #007
+        "answer": "2",
+        "image": "images/isolating_diode.png"
+    },
+]
