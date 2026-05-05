5e8f7d2c5c38c59324336578c28b7dc628bc4123
dontirrigateme <smokey.alley44@gmail.com>
2026-05-05 10:00:51 -0400

    Refactor battery questions for improved readability

--- electude_data/batteries.py
+++ electude_data/batteries.py
@@ -1,10 +1,38 @@
 QUESTIONS = [
     {
-        "question": "Battery sensors are used to measure which parameters?", #01
-        "answer": "electrical current draw, temperature, and voltage",
+        "question": (
+            "Battery sensors are used to measure which parameters?\n\n"
+        ), #001
+        "answer": (
+            "Electrical current draw\n"
+            "Temperature\n"
+            "Voltage"
+        ),
     },
     {
-        "question": "What are the three states that determine a battery's condition?", #02
-        "answer": "state of charge, state of health, state of function",
+        "question": (
+            "What are the three states that determine a battery's condition?\n\n"
+        ), #002
+        "answer": (
+            "State of charge\n"
+            "State of health\n"
+            "State of function"
+        ),
+    },
+    {
+        "question": "The amount of __________ and __________ current is one of the most important parameters that the BMS needs in order to determine the state of charge (SOC).", #003
+        "answer": "charge, discharge",
+    },
+    {
+        "question": "What is the state of charge?", #004
+        "answer": "The percentage of available charge relative to the energy content of a new battery.",
+    },
+    {
+        "question": "What is the state of health?", #005
+        "answer": "The extent to which the current capacity of a charged battery compares to the original capacity specified by the manufacturer, expressed as a percentage.",
+    },
+    {
+        "question": "What is the state of function?", #006
+        "answer": "The expected starting capacity, or the extent to which the battery can perform its function, calculated in the battery management system.",
     },
 ]
