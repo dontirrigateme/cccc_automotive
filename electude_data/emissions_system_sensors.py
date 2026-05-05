557bde0e230a7f0f1f90a9a18037bd390a2fea65
dontirrigateme <smokey.alley44@gmail.com>
2026-05-05 11:25:01 -0400

    Add emissions system sensors questions and answers

---
+++ electude_data/emissions_system_sensors.py
@@ -0,0 +1,18 @@
+#EMISSIONS SYSTEM SENSORS
+
+QUESTIONS = [
+    {
+        "question": (
+            "Battery sensors are used to measure which parameters?"
+        ), #001
+        "answer": (
+            "Electrical current draw\n"
+            "Temperature\n"
+            "Voltage"
+        ),
+    },
+    {
+        "question": "The amount of __________ and __________ current is one of the most important parameters that the BMS needs in order to determine the state of charge (SOC).", #003
+        "answer": "charge, discharge",
+    },
+]
