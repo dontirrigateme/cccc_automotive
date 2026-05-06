05143f25314098f20ca0216c47000e867f4210a8
dontirrigateme <smokey.alley44@gmail.com>
2026-05-05 23:53:59 -0400

    Add multiple questions and answers for NOx sensor

--- electude_data/safety_and_emissions.py
+++ electude_data/safety_and_emissions.py
@@ -87,4 +87,142 @@ QUESTIONS = [
             "C. O2 + HC → CO2 + H2O"
         ),
     },
+    {
+        "question": ( #008
+            "What does the NOx sensor measuere?\n\n"
+            "A. The NOx concentration in the exhaust gas\n"
+            "B. The NOx concentration in the air\n"
+            "C. The oxygen concentration in the exhaust gas\n"
+            "D. The oxygen concentration in the air"
+        ),
+        "answer": (
+            "A. The NOx concentration in the exhaust gas"
+        ),
+    },
+    {
+        "question": ( #009
+            "Where does the NOx sensor measure the NOx concentration?\n\n"
+            "A. After the NOx catalyst\n"
+            "B. Between the three-way catalyst and the NOx catalyst\n"
+            "C. Before the three-way catalyst\n"
+            "D. After the three-way catalyst"
+        ),
+        "answer": (
+            "A. After the NOx catalyst"
+        ),
+    },
+    {
+        "question": ( #010
+            "What is the purpose of the control unit on a NOx sensor?\n\n"
+            "A. It translates the signal from the sensor and activates the actuators\n"
+            "B. It activates the NOx sensor, depending on the measured values\n"
+            "C. It translates the signals of the sensor and passes the information to the powertrain control module"
+        ),
+        "answer": (
+            "C. It translates the signals of the sensor and passes the information to the powertrain control module"
+        ),
+    },
+    {
+        "question": ( #011
+            "Lean mixture engines produce a lot of carbon dioxide (CO2). True or false?"
+        ),
+        "answer": (
+            "False.\n"
+            "Lean mixture engines produce a lot of nitrogen oxides (NOx)."
+        ),
+    },
+    {
+        "question": ( #012
+            "In a mixture that's very rich, which statement is true?\n\n"
+            "A. The exhaust gas contains little oxygen.\n"
+            "B. The exhaust gas contains a lot of oxygen."
+        ),
+        "answer": (
+            "A. The exhaust gas contains little oxygen."
+        ),
+    },
+    {
+        "question": ( #013
+            "The oxygen concentration in the measuring chamber of a NOx sensor must correspond to λ = 1.\n"
+            "What must the pump unit do to achieve this with a rich mixture?\n\n"
+            "A. Pump extra oxygen to the measuring chamber.\n"
+            "B. The pump unit does not have to do anything.\n"
+            "C. Pump oxygen out of the measuring chamber."
+        ),
+        "answer": (
+            "A. Pump extra oxygen to the measuring chamber."
+        ),
+    },
+    {
+        "question": ( #014
+            "In a mixture that's very lean, which statement is true?\n\n"
+            "A. The exhaust gas contains a lot of oxygen.\n"
+            "B. The Exhaust gas contains little oxygen."
+        ),
+        "answer": (
+            "A. The exhaust gas contains a lot of oxygen."
+        ),
+    },
+    {
+        "question": ( #015
+            "The oxygen concentration in the measuring chamber of a NOx sensor must correspond to λ = 1.\n"
+            "What must the pump unit do to achieve this with a lean mixture?\n\n"
+            "A. Pump extra oxygen to the measuring chamber.\n"
+            "B. The pump unit does not have to do anything.\n"
+            "C. Pump oxygen out of the measuring chamber."
+        ),
+        "answer": (
+            "C. Pump oxygen out of the measuring chamber."
+        ),
+    },
+    {
+        "question": ( #016
+            "The NOx sensor consists of two components. What are they?"
+        ),
+        "answer": (
+            "A wideband lambda sensor\n"
+            "A chamber where the NOx concentration is measured"
+        ),
+    },
+    {
+        "question": ( #017
+            "Study the graph.\n"
+            "What happens to the emission of NOx when the mixture is rich?\n\n"
+            "A. The mixture composition has no effect on the NOx emissions\n"
+            "B. Little to no emission of NOx\n"
+            "C. High emission of NOx"
+        ),
+        "answer": (
+            "B. Little to no emission of NOx"
+        ),
+        "image": (
+            "images/nox_sensor_graph.png"
+        ),
+    },
+    {
+        "question": ( #018
+            "Study the graph.\n"
+            "What happens to the emission of NOx when the mixture is lean?\n\n"
+            "A. The mixture composition has no effect on the NOx emissions\n"
+            "B. Little to no emission of NOx\n"
+            "C. High emission of NOx"
+        ),
+        "answer": (
+            "C. High emission of NOx"
+        ),
+        "image": (
+            "images/nox_sensor_graph.png"
+        ),
+    },
+    {
+        "question": ( #019
+            "Why is the NOx sensor not directly connected to the control unit?\n\n"
+            "A. The measured NOx concentration is not used by the control unit.\n"
+            "B. The distance between the NOx sensor and the powertrain control module is too great. The signal would not arrive fast enough at the control unit.\n"
+            "C. The voltage delivered by the sensor is so small that this would be unusable due to external influences."
+        ),
+        "answer": (
+            "C. The voltage delivered by the sensor is so small that this would be unusable due to external influences."
+        ),
+    },
 ]
