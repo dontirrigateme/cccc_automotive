bc8ba4323f8fee8c26253f0d54b15c00b4960d62
dontirrigateme <smokey.alley44@gmail.com>
2026-05-05 23:06:35 -0400

    Update emissions_system_sensors.py

--- electude_data/emissions_system_sensors.py
+++ electude_data/emissions_system_sensors.py
@@ -3,16 +3,88 @@
 QUESTIONS = [
     {
         "question": (
-            "Battery sensors are used to measure which parameters?"
+            "What five gases/substances are produced by combustion?"
         ), #001
         "answer": (
-            "Electrical current draw\n"
-            "Temperature\n"
-            "Voltage"
+            "Carbon dioxide (CO2)\n"
+            "Water (H2O)\n"
+            "Carbon monoxide (CO)\n"
+            "Nitrogen oxides (NOx)\n"
+            "Non-combusted hydrocarbons (HC)"
         ),
     },
     {
-        "question": "The amount of __________ and __________ current is one of the most important parameters that the BMS needs in order to determine the state of charge (SOC).", #003
-        "answer": "charge, discharge",
+        "question": ( #002
+            "What substances are brought together in a combustion engine?\n\n"
+            "A. Air\n"
+            "B. Water\n"
+            "C. Fuel"
+        ),
+        "answer": (
+            "A. Air\n"
+            "C. Fuel"
+        ),
+    },
+    {
+        "question": ( #003
+            "What does air contain?\n\n"
+            "A. Nitrogen (N2)\n"
+            "B. Ammonia (NH3)\n"
+            "C. Oxygen (O2)\n"
+            "D. Carbon (C)"
+        ),
+        "answer": (
+            "A. Nitrogen (N2)\n"
+            "C. Oxygen (O2)"
+        ),
+    },
+    {
+        "question": ( #004
+            "What is the chemical name for fuel?\n\n"
+            "A. Oxygen (O2)\n"
+            "B. Nitrogen (N2)\n"
+            "C. Nitrogen oxides (NOx)\n"
+            "D. Hydrocarbon (HC)"
+        ),
+        "answer": (
+            "D. Hydrocarbon"
+        ),
+    },
+    {
+        "question": ( #005
+            "If combustion is ideal, what substances are produced in the engine?\n\n"
+            "A. Ammonia (NH3)\n"
+            "B. Nitrogen oxides (NOx)\n"
+            "C. Water (H2O)\n"
+            "D. Carbon dioxide (CO2)"
+        ),
+        "answer": (
+            "C. Water (H2O)\n"
+            "D. Carbon dioxide (CO2)"
+        ),
+    },
+    {
+        "question": ( #006
+            "Which of these chemical reactions represents ideal combustion in the engine?\n\n"
+            "A. O2 + HC + NH3 → H2O + NOx\n"
+            "B. CO2 + H2O → NOx + CO\n"
+            "C. O2 + HC → CO2 + H2O\n"
+            "D. CO2 + NOx + HC → O2 + HC + N2"
+        ),
+        "answer": (
+            "C. O2 + HC → CO2 + H2O"
+        ),
+    },
+    {
+        "question": ( #007
+            "Which of these chemical reactions represents ideal combustion in the engine?\n\n"
+            "A. O2 + HC + NH3 → H2O + NOx\n"
+            "B. CO2 + H2O → NOx + CO\n"
+            "C. O2 + HC → CO2 + H2O\n"
+            "D. CO2 + NOx + HC → O2 + HC + N2"
+        ),
+        "answer": (
+            "C. O2 + HC → CO2 + H2O"
+        ),
     },
 ]
