9d2846ec9584fa26a914d3fbaa550dbdf450faee
dontirrigateme <smokey.alley44@gmail.com>
2026-05-08 10:38:33 -0400

    Add advanced auto electricity quiz questions

---
+++ electude_data/advanced_auto_electricity.py
@@ -0,0 +1,186 @@
+# ELECTUDE - ADVANCED AUTO ELECTRICITY
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "Where can you find coils?\n\n"
+            "A. Injectors\n"
+            "B. Speakers\n"
+            "C. Electric motors\n"
+            "D. Relays\n"
+            "E. Transformers\n"
+            "F. All of the above"
+        ),
+        "answer": (
+            "F. All of the above"
+        ),
+    },
+    {
+        "question": ( #002
+            "The unit of self-induction is called:\n\n"
+            "A. Farad (F)\n"
+            "B. Ohms (Ω)\n"
+            "C. Volts (V)\n"
+            "D. Henry (H)"
+        ),
+        "answer": (
+            "D. Henry (H)"
+        ),
+    },
+    {
+        "question": ( #003
+            "What are the types of coils?\n\n"
+            "A. A coil without windings\n"
+            "B. A coil without core\n"
+            "C. A coil with core"
+        ),
+        "answer": (
+            "B. A coil without core\n"
+            "C. A coil with core"
+        ),
+    },
+    {
+        "question": ( #004
+            "Find the following components:\n\n"
+            "Coil with core\n"
+            "Coil without core\n"
+            "Transformer"
+        ),
+        "answer": (
+            "A. Coil without core\n"
+            "B. Coil with core\n"
+            "C. Transformer"
+        ),
+        "image": (
+            "images\types_of_coils.png"
+        ),
+    },
+    {
+        "question": ( #005
+            "Find the correct symbols:\n\n"
+            "Coil symbol with core\n"
+            "Coil symbol without core\n"
+            "Transformer symbol"
+        ),
+        "answer": (
+            "A. Coil symbol without core\n"
+            "B. Coil symbol with core\n"
+            "C. Transformer symbol"
+        ),
+        "image": (
+            "images\coil_transformer_symbols.png"
+        ),
+    },
+    {
+        "question": ( #006
+            "Each coil has a constant. What do you call this constant?\n\n"
+            "A. Capacitance\n"
+            "B. Resistance\n"
+            "C. Electricity\n"
+            "D. Coefficient of self-induction"
+        ),
+        "answer": (
+            "D. Coefficient of self-induction"
+        ),
+    },
+    {
+        "question": ( #007
+            "How is a coil made?\n\n"
+            "A. By spooling an insulator into the shape of a coil\n"
+            "B. By spooling an electric conductor into the shape of a coil\n"
+            "C. By stamping material with a coiol-shaped object"
+        ),
+        "answer": (
+            "B. By spooling an electric conductor into the shape of a coil"
+        ),
+    },
+    {
+        "question": ( #008
+            "Find the following parts of a coil:\n\n"
+            "Connections\n"
+            "Core\n"
+            "Windings"
+        ),
+        "answer": (
+            "A. Core\n"
+            "B. Connections\n"
+            "C. Windings"
+        ),
+        "image": (
+            "images\parts_of_a_coil.png"
+        ),
+    },
+    {
+        "question": ( #009
+            "How is the property of a core expressed?\n\n"
+            "A. Permeability\n"
+            "B. Remanence\n"
+            "C. Induction"
+        ),
+        "answer": (
+            "A. Permeability"
+        ),
+    },
+    {
+        "question": ( #010
+            "Which letter represents permeability?\n\n"
+            "A. Φ (pronounced as phi)\n"
+            "B. μ (pronounced as mu)"
+        ),
+        "answer": (
+            "B. μ (pronounced as mu)"
+        ),
+    },
+    {
+        "question": ( #011
+            "What indicates permeability?\n\n"
+            "A. The thermal permeability of a material\n"
+            "B. The magnetic permeability of a material\n"
+            "C. The optical permeability of a material"
+        ),
+        "answer": (
+            "B. The magnetic permeability of a material"
+        ),
+    },
+    {
+        "question": ( #012
+            "What defines the coefficient of self-induction in a coil?\n\n"
+            "A. The core length\n"
+            "B. The core cross-section\n"
+            "C. The number of turns\n"
+            "D. The properties of the core\n"
+            "E. All of the above"
+        ),
+        "answer": (
+            "E. All of the above"
+        ),
+    },
+    {
+        "question": ( #013
+            "What does the resistance of a coil depend on?\n\n"
+            "A. The coefficient of self-induction\n"
+            "B. The frequency\n"
+            "C. The current\n"
+            "D. The voltage\n"
+            "E. All of the above"
+        ),
+        "answer": (
+            "A. The coefficient of self-induction\n"
+            "B. The frequency"
+        ),
+    },
+    {
+        "question": ( #014
+            "The resistance of a coil depends on the frequency.\n"
+            "What can a coil be used for?\n\n"
+            "A. To block DC voltage\n"
+            "B. As a filter to block high frequencies\n"
+            "C. To suppress interfering signals\n"
+            "D. All of the above"
+        ),
+        "answer": (
+            "B. As a filter to block high frequencies\n"
+            "C. To suppress interfering signals"
+        ),
+    },
+]
