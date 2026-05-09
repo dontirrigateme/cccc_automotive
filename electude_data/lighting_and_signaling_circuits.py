38e86ae5b3032e0bcc5e03c85c11fd5730f4f8d9
dontirrigateme <smokey.alley44@gmail.com>
2026-05-08 22:28:27 -0400

    Add multiple questions and answers about headlights

--- electude_data/lighting_and_signaling_circuits.py
+++ electude_data/lighting_and_signaling_circuits.py
@@ -115,4 +115,240 @@ QUESTIONS = [
         "question": "Why are headlights sprayed with adhesive primer during refurbishing?", #27
         "answer": "it allows the new UV coating to adhere to the polycarbonate of the headlight",
     },
+    {
+        "question": ( #028
+            "What type of headlight is depicted in image A?\n\n"
+            "A. Projection module\n"
+            "B. Reflector module"
+        ),
+        "answer": (
+            "B. Reflector module"
+        ),
+        "image": (
+            images/types_of_headlights.png"
+        ),
+    },
+    {
+        "question": ( #029
+            "What type of headlight is depicted in image B?\n\n"
+            "A. Projection module\n"
+            "B. Reflector module"
+        ),
+        "answer": (
+            "B. Projection module"
+        ),
+        "image": (
+            images/types_of_headlights.png"
+        ),
+    },
+    {
+        "question": ( #030
+            "Which component of the reflector headlamp is the reflector?"
+        ),
+        "answer": (
+            "B."
+        ),
+        "image": (
+            "images/reflector_headlamp.png"
+        ),
+    },
+    {
+        "question": ( #031
+            "Which component of the reflector headlamp is the halogen lamp?"
+        ),
+        "answer": (
+            "A."
+        ),
+        "image": (
+            "images/reflector_headlamp.png"
+        ),
+    },
+    {
+        "question": ( #032
+            "Which component of the projection headlamp is the lens?"
+        ),
+        "answer": (
+            "B."
+        ),
+        "image": (
+            "images/projection_headlamp.png"
+        ),
+    },
+    {
+        "question": ( #033
+            "Which component of the projection headlamp is the light-beam guard?"
+        ),
+        "answer": (
+            "A."
+        ),
+        "image": (
+            "images/projection_headlamp.png"
+        ),
+    },
+    {
+        "question": ( #034
+            "What are the advantages of the projector headlamp?\n\n"
+            "A. Greater light output\n"
+            "B. Smaller installation space required\n"
+            "C. The high beam won't blind oncoming traffic\n"
+            "D. Better separation of light and dark"
+        ),
+        "answer": (
+            "A. Greater light output\n"
+            "B. Smaller installation space required\n"
+            "D. Better separation of light and dark"
+        ),
+    },
+    {
+        "question": ( #035
+            "Which reflector headlight is the paraboloid headlamp?"
+        ),
+        "answer": (
+            "B."
+        ),
+        "image": (
+            "images/types_of_reflector_headlights.png"
+        ),
+    },
+    {
+        "question": ( #036
+            "Which reflector headlight is the functional faceted headlamp?"
+        ),
+        "answer": (
+            "A."
+        ),
+        "image": (
+            "images/types_of_reflector_headlights.png"
+        ),
+    },
+    {
+        "question": ( #037
+            "Which component of the reflector headlight is the lamp glass?"
+        ),
+        "answer": (
+            "C."
+        ),
+        "image": (
+            "images/reflector_headlight_components.png"
+        ),
+    },
+    {
+        "question": ( #038
+            "Which component of the reflector headlight is the halogen lamp?"
+        ),
+        "answer": (
+            "A."
+        ),
+        "image": (
+            "images/reflector_headlight_components.png"
+        ),
+    },
+    {
+        "question": ( #039
+            "Which component of the reflector headlight is the reflector?"
+        ),
+        "answer": (
+            "B."
+        ),
+        "image": (
+            "images/reflector_headlight_components.png"
+        ),
+    },
+    {
+        "question": ( #040
+            "Which reflector headlight shows the paraboloid headlamp?"
+        ),
+        "answer": (
+            "B."
+        ),
+        "image": (
+            "images/paraboloid_vs_functional_faceted.png"
+        ),
+    },
+    {
+        "question": ( #041
+            "Which reflector headlight shows the faceted headlamp?"
+        ),
+        "answer": (
+            "A."
+        ),
+        "image": (
+            "images/paraboloid_vs_functional_faceted.png"
+        ),
+    },
+    {
+        "question": ( #042
+            "What are the differences between the two reflector headlamps\n\n?"
+            "A. The faceted headlamp has a larger reflector surface\n"
+            "B. The faceted headlamp has clear headlamp glass\n"
+            "C. The paraboloid headlamp has profiled headlamp glass\n"
+            "D. The faceted headlamp is the oldest system\n"
+            "E. The paraboloid headlamp has several reflector surfaces\n"
+            "F. All of the above"
+        ),
+        "answer": (
+            "A. The faceted headlamp has a larger reflector surface\n"
+            "B. The faceted headlamp has clear headlamp glass\n"
+            "C. The paraboloid headlamp has profiled headlamp glass"
+        ),
+    },
+    {
+        "question": ( #043
+            "Which projector headlight is superellipsoid?"
+        ),
+        "answer": (
+            "A."
+        ),
+        "image": (
+            "images/types_of_projector_headlights.png"
+        ),
+    },
+    {
+        "question": ( #044
+            "Which projector headlight is ellipsoid?"
+        ),
+        "answer": (
+            "B."
+        ),
+        "image": (
+            "images/types_of_projector_headlights.png"
+        ),
+    },
+    {
+        "question": ( #045
+            "Which projector light beam is from an ellipsoid headlamp?"
+        ),
+        "answer": (
+            "B."
+        ),
+        "image": (
+            "images/ellipsoid_vs_superellipsoid.png"
+        ),
+    },
+    {
+        "question": ( #046
+            "Which projector light beam is from a superellipsoid headlamp?"
+        ),
+        "answer": (
+            "A."
+        ),
+        "image": (
+            "images/ellipsoid_vs_superellipsoid.png"
+        ),
+    },
+    {
+        "question": ( #047
+            "What are the differences between the two projector headlamps\n\n?"
+            "A. The superellipsoid headlamp is smaller\n"
+            "B. The superellipsoid headlamp is has a larger reflector surface\n"
+            "C. The superellipsoid headlamp has a higher light output\n"
+            "D. The superellipsoid headlamp has clear headlamp glass\n"
+            "E. All of the above"
+        ),
+        "answer": (
+            "B. The superellipsoid headlamp is has a larger reflector surface\n"
+            "C. The superellipsoid headlamp has a higher light output\n"
+            "D. The superellipsoid headlamp has clear headlamp glass"
+        ),
+    },
 ]
