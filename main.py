4da6fc7bbdfb5d0f78c1d88a142e3993952c97e9
dontirrigateme <smokey.alley44@gmail.com>
2026-05-11 03:47:53 -0400

    Add study guide questions to main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from electude_data.charging_system_theory import QUESTIONS as charging_system_th
 from electude_data.safety_and_emissions import QUESTIONS as safety_and_emissions_questions
 from electude_data.series_and_parallel_circuits import QUESTIONS as series_and_parallel_circuits_questions
 from electude_data.coils_and_relays import QUESTIONS as coils_and_relays_questions
+from electude_data.study_guide_2026 import QUESTIONS as study_guide_2026_questions
 from ase_data.ch1_1_engine_diagnosis import QUESTIONS as ch1_1_engine_diagnosis_questions
 from ase_data.ch1_2_cylinder_head_valve_train import QUESTIONS as ch1_2_cylinder_head_valve_train_questions
 from ase_data.ch1_3_engine_block_diagnosis import QUESTIONS as ch1_3_engine_block_diagnosis_questions
@@ -83,6 +84,10 @@ ELECTUDE_TOPICS = {
     "coils_and_relays": {
         "label": "Coils & Relays",
         "questions": coils_and_relays_questions,
+    },
+    "study_guide_2026": {
+        "label": "Study Guide",
+        "questions": study_guide_2026_questions,
     },
 }
 
@@ -189,6 +194,7 @@ class FlashcardView(discord.ui.View):
     app_commands.Choice(name="Safety & Emissions", value="safety_and_emissions"),
     app_commands.Choice(name="Series & Parallel Circuits", value="series_and_parallel_circuits"),
     app_commands.Choice(name="Coils & Relays", value="coils_and_relays"),
+    app_commands.Choice(name="Study Guide", value="study_guide_2026"),
 ])
 async def electude(interaction: discord.Interaction, topic: app_commands.Choice[str]):
 
