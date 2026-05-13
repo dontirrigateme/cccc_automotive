8b35572768b04a382f1a4ac32c3a1bd0f4e8b627
dontirrigateme <smokey.alley44@gmail.com>
2026-05-12 21:42:32 -0400

    Add fuel injection system diagnosis study guide

---
+++ study_guide_data/fuel_inj_sys_diag_sg.py
@@ -0,0 +1,348 @@
+# AUT-181 Engine Performance 1 - FUEL INJECTION SYSTEM DIAGNOSIS & SERVICE STUDY GUIDE
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "Always pressurize the fuel system before disconnecting any fuel system components.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False"
+        ),
+    },
+    {
+        "question": ( #002
+            "Never turn on the ignition switch when any fuel system component is disconnected.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #003
+            "The engine must be at normal operating temperature before the O2 sensor is tested.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #004
+            "The engine must be at normal operating temperature before the ECT sensor is tested.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False"
+        ),
+    },
+    {
+        "question": ( #005
+            "A good injector makes a rhythmic clicking sound as the injector is energized and de-energized several times each second.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #006
+            "Creating a large vacuum leak will cause the short-term fuel trim (STFT) and long-term fuel trim (LTFT) to decrease.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False"
+        ),
+    },
+    {
+        "question": ( #007
+            "STFT adjustments are temporary and are not stored in the PCM’s memory.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #008
+            "The LTFT works to bring the STFT close to a 0 percent correction.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #009
+            "Which of the following sensors is used to modify the engine’s air/fuel mixture based on oxygen in the exhaust?\n\n"
+            "A. TP sensor\n"
+            "B. MAP sensor\n"
+            "C. O2 sensor\n"
+            "D. MAF sensor"
+        ),
+        "answer": (
+            "C. O2 sensor"
+        ),
+    },
+    {
+        "question": ( #010
+            "Fuel trim is being discussed. Technician A says a + 12.0 percent LTFT value indicates that the system is running 12 percent richer than normal. Technician B says LTFT should constantly change between positive and negative values when the system is running in closed loop. Who is correct?\n\n"
+            "A. Tech A only\n"
+            "B. Tech B only\n"
+            "C. Both tech A and tech B\n"
+            "D. Neither tech A nor tech B"
+        ),
+        "answer": (
+            "D. Neither tech A nor tech B"
+        ),
+    },
+    {
+        "question": ( #011
+            "An oxygen sensor must heat up to approximately __________ before functioning in its closed loop mode.\n\n"
+            "A. 600°F (316°C)\n"
+            "B. 800°F (427°C)\n"
+            "C. 400°F (204°C)\n"
+            "D. 200°F (93°C)"
+        ),
+        "answer": (
+            "A. 600°F (316°C)"
+        ),
+    },
+    {
+        "question": ( #012
+            "Which of the following should be checked before proceeding with specific fuel injection and electronic control testing?\n\n"
+            "A. PCV system\n"
+            "B. Battery\n"
+            "C. Engine coolant level\n"
+            "D. All the answers are correct"
+        ),
+        "answer": (
+            "D. All the answers are correct"
+        ),
+    },
+    {
+        "question": ( #013
+            "Most heated oxygen sensors have __________ wire(s) connected to them.\n\n"
+            "A. Three\n"
+            "B. Four\n"
+            "C. One\n"
+            "D. Two"
+        ),
+        "answer": (
+            "B. Four"
+        ),
+    },
+    {
+        "question": ( #014
+            "Operation of an O2 sensor is checked with a(n) __________.\n\n"
+            "A. Voltmeter\n"
+            "B. Ammeter\n"
+            "C. Ohmmeter\n"
+            "D. All the answers are correct"
+        ),
+        "answer": (
+            "A. Voltmeter"
+        ),
+    },
+    {
+        "question": ( #015
+            "The voltage signal from most O2 sensors varies between __________ and __________ volt(s).\n\n"
+            "A. 3 and 5\n"
+            "B. 2 and 3\n"
+            "C. 0 and 1\n"
+            "D. 9.6 and 12"
+        ),
+        "answer": (
+            "C. 0 and 1"
+        ),
+    },
+    {
+        "question": ( #016
+            "The activity of an O2 sensor is best monitored with a\n\n"
+            "A. Scan tool\n"
+            "B. Noid light\n"
+            "C. Lab scope\n"
+            "D. Logic probe"
+        ),
+        "answer": (
+            "C. Lab scope"
+        ),
+    },
+    {
+        "question": ( #017
+            "Technician A says a defective injector may cause cylinder misfiring at idle speed. Technician B says restricted injector tips may result in acceleration stumbles. Who is correct?\n\n"
+            "A. Tech A only\n"
+            "B. Tech B only\n"
+            "C. Both tech A and tech B\n"
+            "D. Neither tech A nor tech B"
+        ),
+        "answer": (
+            "C. Both tech A and tech B"
+        ),
+    },
+    {
+        "question": ( #018
+            "Technician A says a rich air/fuel ratio may be caused by low fuel pump pressure. Technician B says a rich air/fuel ratio may be caused by a defective engine coolant temperature sensor. Who is correct?\n\n"
+            "A. Tech A only\n"
+            "B. Tech B only\n"
+            "C. Both tech A and tech B\n"
+            "D. Neither tech A nor tech B"
+        ),
+        "answer": (
+            "B. Tech B only"
+        ),
+    },
+    {
+        "question": ( #019
+            "A rich air/fuel mixture is being discussed. Technician A says oxygen content in the exhaust will be high. Technician B says the O2 sensor voltage will be high. Who is correct?\n\n"
+            "A. Tech A only\n"
+            "B. Tech B only\n"
+            "C. Both tech A and tech B\n"
+            "D. Neither tech A nor tech B"
+        ),
+        "answer": (
+            "B. Tech B only"
+        ),
+    },
+    {
+        "question": ( #020
+            "Idle learn procedures for ETC (throttle-by-wire)-equipped vehicles are being discussed. Technician A says the learn or relearn procedure should be completed anytime the PCM has been replaced or updated, or after the throttle body has been cleaned or replaced. Technician B says the system does not need to relearn if the battery has been disconnected. The system will automatically go through the procedure when the engine is restarted.Who is correct?\n\n"
+            "A. Tech A only\n"
+            "B. Tech B only\n"
+            "C. Both tech A and tech B\n"
+            "D. Neither tech A nor tech B"
+        ),
+        "answer": (
+            "C. Both tech A and tech B"
+        ),
+    },
+    {
+        "question": ( #021
+            "Technician A says if a lean condition caused by a vacuum leak, restricted injectors, dirty MAF, clogged fuel filter, or bad fuel pump exists, the LTFT will have a negative number. Technician B says when the O2 sensor has a rich signal, the STFT goes lean to adjust the air/fuel ratio. Who is correct?\n\n"
+            "A. Tech A only\n"
+            "B. Tech B only\n"
+            "C. Both tech A and tech B\n"
+            "D. Neither tech A nor tech B"
+        ),
+        "answer": (
+            "B. Tech B only"
+        ),
+    },
+    {
+        "question": ( #022
+            "Which of the following would NOT cause the fuel trim to be out of range?\n\n"
+            "A. Faulty fuel pressure regulator\n"
+            "B. Faulty idle air control (IAC) motor\n"
+            "C. Restricted fuel filter\n"
+            "D. Faulty mass airflow (MAF) sensor"
+        ),
+        "answer": (
+            "B. Faulty idle air control (IAC) motor"
+        ),
+    },
+    {
+        "question": ( #023
+            "A technician is testing a fuel injection system with a scan tool. While the engine is running in closed-loop, he loosens a vacuum hose to create a small vacuum leak. Initially the STFT rises and the LTFT remains unchanged. After a few minutes the STFT returns to near zero and the LTFT rises. This test proves that __________.\n\n"
+            "A. The system is working normally\n"
+            "B. The system is running lean\n"
+            "C. The O2 sensor is faulty\n"
+            "D. The system is running rich"
+        ),
+        "answer": (
+            "A. The system is working normally"
+        ),
+    },
+    {
+        "question": ( #024
+            "A/F ratio sensors are being discussed.Technician A says the output voltage of these sensors is much higher than a conventional O2S (oxygen sensor). Technician B say when the output of an A/F ratio sensor is looked at with an OBD-II generic scan tool, the voltage range will appear the same as a conventional O2S sensor, 0 to 1 volt. Who is correct?\n\n"
+            "A. Tech A only\n"
+            "B. Tech B only\n"
+            "C. Both tech A and tech B\n"
+            "D. Neither tech A nor tech B"
+        ),
+        "answer": (
+            "C. Both tech A and tech B"
+        ),
+    },
+    {
+        "question": ( #025
+            "Technician A says a small leak in the air duct between the MAF and the throttle body will not cause a driveability problem. Technician B says a small intake manifold leak in a speed density system will not cause a driveability problem. Who is correct?\n\n"
+            "A. Tech A only\n"
+            "B. Tech B only\n"
+            "C. Both tech A and tech B\n"
+            "D. Neither tech A nor tech B"
+        ),
+        "answer": (
+            "B. Tech B only"
+        ),
+    },
+    {
+        "question": ( #026
+            "A vehicle with excessively high fuel pressure is being diagnosed. Technician A says the problem could be a restricted fuel return line. Technician B says the problem could be a shorted (closed) fuel pump relay. Who is correct?\n\n"
+            "A. Tech A only\n"
+            "B. Tech B only\n"
+            "C. Both tech A and tech B\n"
+            "D. Neither tech A nor tech B"
+        ),
+        "answer": (
+            "A. Tech A only"
+        ),
+    },
+    {
+        "question": ( #027
+            "Which of the following components can cause excessively high pressure in a returnless fuel delivery system?\n\n"
+            "A. Faulty fuel pressure regulator\n"
+            "B. Faulty fuel pressure sensor\n"
+            "C. Faulty PCM\n"
+            "D. All the answers are correct"
+        ),
+        "answer": (
+            "D. All the answers are correct"
+        ),
+    },
+    {
+        "question": ( #028
+            "The best tool to verify that the engine is running excessively rich or lean is a(n) __________.\n\n"
+            "A. Lab scope\n"
+            "B. Exhaust gas analyzer\n"
+            "C. Fuel pressure gauge\n"
+            "D. Manifold vacuum gauge"
+        ),
+        "answer": (
+            "B. Exhaust gas analyzer"
+        ),
+    },
+    {
+        "question": ( #029
+            "When viewing injector patterns on a lab scope, the injector pulse end with a __________.\n\n"
+            "A. Downward right angle\n"
+            "B. Last upward spike\n"
+            "C. First upward spike\n"
+            "D. Highest upward spike"
+        ),
+        "answer": (
+            "B. Last upward spike"
+        ),
+    },
+    {
+        "question": ( #030
+            "An accelerator pedal position (APP) sensor is found on vehicles with __________.\n\n"
+            "A. Automatic traction control\n"
+            "B. Throttle-by-wire\n"
+            "C. Automatic transmissions\n"
+            "D. Gasoline direct injection (GDI)"
+        ),
+        "answer": (
+            "B. Throttle-by-wire"
+        ),
+    },
+]
