3496060d548c38e6d550f3aa2e3c5961baf3ad74
dontirrigateme <smokey.alley44@gmail.com>
2026-06-16 03:44:30 -0400

    Refactor braking system theory questions and answers

---
+++ electude_data/elec_braking_sys_theory.py
@@ -0,0 +1,560 @@
+# ELECTUDE - ELECTRONIC BRAKING SYSTEMS THEORY
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "Which system ensures that the wheels do not lock, or minimizes locking, during an emergency stop?\n\n"
+            "A. Anti-Slip Regulation (ASR)\n"
+            "B. Brake Assist System (BAS)\n"
+            "C. Anti-Lock Braking System (ABS)\n"
+            "D. Motor Speed Regulation (MSR)"
+        ),
+        "answer": (
+            "C. Anti-Lock Braking System (ABS)"
+        ),
+    },
+    {
+        "question": ( #002
+            "What does the Anti-Lock Braking System (ABS) control?\n\n"
+            "A. The transmission\n"
+            "B. The engine\n"
+            "C. The wheels\n"
+            "D. The brakes"
+        ),
+        "answer": (
+            "D. The brakes"
+        ),
+    },
+    {
+        "question": ( #003
+            "What is (are) the advantage(s) of the Anti-Lock Braking System (ABS)?\n\n"
+            "A. The vehicle has a shorter braking distance\n"
+            "B. Better fuel economy\n"
+            "C. Less tire wear (flat spots) during an emergency stop, because the wheels don't lock\n"
+            "D. The vehicle steering can still be controlled during braking\n"
+            "E. A and B\n"
+            "F. B, C, and D\n"
+            "G. A, C, and D\n"
+            "H. All of the above\n"
+            "I. None of the above\n"
+        ),
+        "answer": (
+            "G. A, C, and D"
+        ),
+    },
+    {
+        "question": ( #004
+            "Which system minimizes wheel slip during rapid acceleration by reducing the engine power?\n\n"
+            "A. Brake Assist System (BAS)\n"
+            "B. Anti-Lock Braking System (ABS)\n"
+            "C. Anti-Slip Regulation (ASR)\n"
+            "D. Traction Control System (TCS)"
+        ),
+        "answer": (
+            "C. Anti-Slip Regulation (ASR)"
+        ),
+    },
+    {
+        "question": ( #005
+            "What does Anti-Slip Regulation (ASR) control?\n\n"
+            "A. The braking pressure\n"
+            "B. The transmission\n"
+            "C. The brakes\n"
+            "D. The engine"
+        ),
+        "answer": (
+            "D. The engine"
+        ),
+    },
+    {
+        "question": ( #006
+            "What is (are) the advantage(s) of Anti-Slip Regulation (ASR)?\n\n"
+            "A. Less tire wear\n"
+            "B. Less strain on the powertrain\n"
+            "C. The vehicle has a shorter braking distance\n"
+            "D. Less brake wear\n"
+            "E. A and B\n"
+            "F. C and D\n"
+            "G. All of the above\n"
+            "H. None of the above"
+        ),
+        "answer": (
+            "E. A and B"
+        ),
+    },
+    {
+        "question": ( #007
+            "Which system ensures that the vehicle has the optimum braking pressure during an emergency stop?\n\n"
+            "A. Brake Assist System (BAS)\n"
+            "B. Traction Control System (TCS)\n"
+            "C. Anti-Lock Braking System (ABS)\n"
+            "D. Electronic Stability Control (ESC)"
+        ),
+        "answer": (
+            "A. Brake Assist System (BAS)"
+        ),
+    },
+    {
+        "question": ( #008
+            "What does the Brake Assist System (BAS) control?\n\n"
+            "A. The transmission\n"
+            "B. The engine\n"
+            "C. The braking system\n"
+            "D. The wheels"
+        ),
+        "answer": (
+            "C. The braking system"
+        ),
+    },
+    {
+        "question": ( #009
+            "What is (are) the advantage(s) of the Brake Assist System (BAS)?\n\n"
+            "A. Better fuel economy\n"
+            "B. Less wear on the brake pads\n"
+            "C. Increased braking pressure during an emergency stop\n"
+            "D. Reduces brake pressure so tires don't slip\n"
+            "E. A and B\n"
+            "F. A and D\n"
+            "G. All of the above\n"
+            "H. None of the above"
+        ),
+        "answer": (
+            "C. Increased braking pressure during an emergency stop"
+        ),
+    },
+    {
+        "question": ( #010
+            "Which system ensures that the wheels do not slip when letting off of the throttle during deceleration?\n\n"
+            "A. Electronic Stability Control (ESC)\n"
+            "B. Anti-Slip Regulation (ASR)\n"
+            "C. Traction Control System (TCS)\n"
+            "D. Motor Speed Regulation (MSR)"
+        ),
+        "answer": (
+            "D. Motor Speed Regulation (MSR)"
+        ),
+    },
+    {
+        "question": ( #011
+            "What does Motor Speed Regulation (MSR) control?\n\n"
+            "A. The engine\n"
+            "B. The ignition\n"
+            "C. The transmission\n"
+            "D. The brakes"
+        ),
+        "answer": (
+            "A. The engine"
+        ),
+    },
+    {
+        "question": ( #012
+            "What is (are) the advantage(s) of Motor Speed Regulation (MSR)?\n\n"
+            "A. Less wear and tear on the transmission\n"
+            "B. The driven wheels do not slip\n"
+            "C. The brakes suffer less wear\n"
+            "D. A and C\n"
+            "E. B and C\n"
+            "F. All of the above\n"
+            "G. None of the above"
+        ),
+        "answer": (
+            "B. The driven wheels do not slip"
+        ),
+    },
+    {
+        "question": ( #013
+            "Which system ensures that one wheel is braked as soon as it loses grip?\n\n"
+            "A. Traction Control System (TCS)\n"
+            "B. Electronic Stability Control (ESC)\n"
+            "C. Motor Speed Regulation (MSR)\n"
+            "D. Anti-Lock Braking System (ABS)"
+        ),
+        "answer": (
+            "A. Traction Control System (TCS)"
+        ),
+    },
+    {
+        "question": ( #014
+            "What does the Traction Control System (TCS) control?\n\n"
+            "A. The engine\n"
+            "B. TCS brakes the wheel that is slipping\n"
+            "C. TCS brakes the wheel that has grip\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "B. TCS brakes the wheel that is slipping"
+        ),
+    },
+    {
+        "question": ( #015
+            "What is (are) the advantage(s) of the Traction Control System (TCS)?\n\n"
+            "A. A vehicle with non-locking differential can pull away from a stop when one wheel begins to slip\n"
+            "B. Less wear of the brakes\n"
+            "C. Less braking power is required\n"
+            "D. A and B\n"
+            "E. B and C\n"
+            "F. All of the above\n"
+            "G. None of the above"
+        ),
+        "answer": (
+            "A. A vehicle with non-locking differential can pull away from a stop when one wheel begins to slip"
+        ),
+    },
+    {
+        "question": ( #016
+            "Which system helps the driver maintain control of the vehicle (e.g., if heading into a corner too fast)?\n\n"
+            "A. Electronic Stability Control (ESC)\n"
+            "B. Anti-Slip Regulation (ASR)\n"
+            "C. Anti-Lock Braking System (ABS)\n"
+            "D. Brake Assist System (BAS)"
+        ),
+        "answer": (
+            "A. Electronic Stability Control (ESC)"
+        ),
+    },
+    {
+        "question": ( #017
+            "What does Electronic Stability Control (ESC) control?\n\n"
+            "A. The transmission\n"
+            "B. The engine power\n"
+            "C. The brakes\n"
+            "D. A and B\n"
+            "E. B and C\n"
+            "F. All of the above\n"
+            "G. None of the above"
+        ),
+        "answer": (
+            "E. B and C"
+        ),
+    },
+    {
+        "question": ( #018
+            "Does Electronic Stability Control (ESC) only intervene if the driver uses the brakes?\n\n"
+            "A. Yes\n"
+            "B. No"
+        ),
+        "answer": (
+            "B. No\n"
+            "ESC intervenes without any action from the driver"
+        ),
+    },
+    {
+        "question": ( #019
+            "What is (are) the advantage(s) of Electronic Stability Control (ESC)?\n\n"
+            "A. The vehicle is less likely to go out of control\n"
+            "B. The brakes suffer less wear\n"
+            "C. The vehicle can still be controlled\n"
+            "D. A and B\n"
+            "E. A and C\n"
+            "F. All of the above\n"
+            "G. None of the above"
+        ),
+        "answer": (
+            "E. A and C"
+        ),
+    },
+    {
+        "question": ( #020
+            "What extra purpose does the active brake booster have compared with a normal brake booster?\n\n"
+            "A. It reduces the braking pressure for a normal braking action\n"
+            "B. It increases the vacuum of the brake booster\n"
+            "C. It increases the braking pressure before Electronic Stability Control (ESC) acts"
+        ),
+        "answer": (
+            "C. It increases the braking pressure before Electronic Stability Control (ESC) acts"
+        ),
+    },
+    {
+        "question": ( #021
+            "What happens when Electronic Stability Control (ESC) activates the brake magnet valve to build up pressure?\n\n"
+            "A. A magnetic field is built up in the coil of the brake magnet valve\n"
+            "B. The coil pushes itself through the magnetic field to the left\n"
+            "C. The metal core of the brake magnet valve is repelled\n"
+            "D. All of the above\n"
+            "E. None of the above"
+        ),
+        "answer": (
+            "D. All of the above"
+        ),
+    },
+    {
+        "question": ( #022
+            "What happens when Electronic Stability Control activates the brake magnet valve to build up pressure?\n\n"
+            "A. The vacuum passage is open\n"
+            "B. Atmospheric air pressure enters the pressure chamber\n"
+            "C. A pressure difference develops between the working chamber and the vacuum chamber\n"
+            "D. A and B\n"
+            "E. B and C\n"
+            "F. All of the above\n"
+            "G. None of the above"
+        ),
+        "answer": (
+            "E. B and C"
+        ),
+    },
+    {
+        "question": ( #023
+            "Which system is involved in giving the vehicle more stability in an emergency situation?\n\n"
+            "A. Brake Assist System (BAS)\n"
+            "B. Anti-Lock Braking System (ABS)\n"
+            "C. Electronic Stability Control (ESC)\n"
+            "D. Anti-Slip Regulation (ASR)"
+        ),
+        "answer": (
+            "C. Electronic Stability Control (ESC)"
+        ),
+    },
+    {
+        "question": ( #024
+            "Why are there arrows on the acceleration sensor?\n\n"
+            "A. The arrows indicate where the acceleration sensor must be installed\n"
+            "B. The arrows must always be installed in the longitudinal direction\n"
+            "C. The arrows must always be installed in the transverse direction\n"
+            "D. The sensor measures acceleration along this axis"
+        ),
+        "answer": (
+            "D. The sensor measures acceleration along this axis"
+        ),
+        "image": (
+            "images/acceleration_sensor.png",
+        ),
+    },
+    {
+        "question": ( #025
+            "Which task does the acceleration sensor perform?\n\n"
+            "A. It measures the speed of the vehicle\n"
+            "B. It measures vehicle acceleration in a specific direction\n"
+            "C. It measures the outside air temperature\n"
+            "D. It measures the vehicle's brake force"
+        ),
+        "answer": (
+            "B. It measures vehicle acceleration in a specific direction"
+        ),
+    },
+    {
+        "question": ( #026
+            "Why are there multiple acceleration sensors in a vehicle?\n\n"
+            "A. Because the sensor can only register acceleration along one axis\n"
+            "B. When a sensor is faulty, another sensor will take over its task\n"
+            "C. Because the sensor can only register acceleration in four directions"
+        ),
+        "answer": (
+            "A. Because the sensor can only register acceleration along one axis"
+        ),
+    },
+    {
+        "question": ( #027
+            "In which directions can a vehicle accelerate?\n\n"
+            "A. Transverse direction\n"
+            "B. Elevation direction\n"
+            "C. Longitudinal direction\n"
+            "D. All of the above\n"
+            "E. None of the above"
+        ),
+        "answer": (
+            "D. All of the above"
+        ),
+    },
+    {
+        "question": ( #028
+            "True or false: Rear-wheel drive means a car can end up in an oversteer situation.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #029
+            "True or false: Front-wheel drive means a car can end up in an understeer situation.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #030
+            "Which component(s) ensure(s) that sufficient pressure is built up in a normal braking situation?\n\n"
+            "A. Master cylinder\n"
+            "B. Hydraulic valve block\n"
+            "C. Brake booster\n"
+            "D. High-pressure pump\n"
+            "E. A and C\n"
+            "F. B and D\n"
+            "G. All of the above\n"
+            "H. None of the above"
+        ),
+        "answer": (
+            "E. A and C"
+        ),
+    },
+    {
+        "question": ( #031
+            "Which component(s) ensure(s) that pressure is built up in the event of an Electronic Stability Control (ESC) braking intervention?\n\n"
+            "A. Hydraulic valve block\n"
+            "B. High-pressure pump\n"
+            "C. Solenoid valve\n"
+            "D. Master cylinder\n"
+            "E. A and C\n"
+            "F. A, B, and C\n"
+            "G. C, D, and E\n"
+            "H. All of the above\n"
+            "I. None of the above"
+        ),
+        "answer": (
+            "G. C, D, and E"
+        ),
+    },
+    {
+        "question": ( #032
+            "Which sensor(s) recognize the driver's intended course?\n\n"
+            "A. Lateral acceleration sensor\n"
+            "B. Angular speed sensor\n"
+            "C. Speed sensors\n"
+            "D. Steering angle sensor\n"
+            "E. A and B\n"
+            "F. B and C\n"
+            "G. C and D\n"
+            "H. All of the above\n"
+            "I. None of the above"
+        ),
+        "answer": (
+            "G. C and D"
+        ),
+    },
+    {
+        "question": ( #033
+            "Which sensor(s) recognize the actual course?\n\n"
+            "A. Yaw sensor\n"
+            "B. Accelerator pedal sensor\n"
+            "C. Lateral acceleration sensor\n"
+            "D. Wheel speed sensor\n"
+            "E. A and B\n"
+            "F. A and C\n"
+            "G. B and D\n"
+            "H. All of the above\n"
+            "I. None of the above"
+        ),
+        "answer": (
+            "F. A and C"
+        ),
+    },
+    {
+        "question": ( #034
+            "What is the purpose of the yaw sensor?\n\n"
+            "A. To measure the braking forces on the vehicle\n"
+            "B. To measure the angular speed of the vehicle\n"
+            "C. To measure the speed of the vehicle\n"
+            "D. To measure the sideways (lateral) forces on the vehicle"
+        ),
+        "answer": (
+            "B. To measure the angular speed of the vehicle"
+        ),
+    },
+    {
+        "question": ( #035
+            "What is the first thing Electronic Stability Control (ESC) does to try and keep the vehicle on the desired course?\n\n"
+            "A. Brakes all four wheels\n"
+            "B. Reduces the engine power\n"
+            "C. Brakes one wheel"
+        ),
+        "answer": (
+            "B. Reduces the engine power"
+        ),
+    },
+    {
+        "question": ( #036
+            "True or false: The inductive wheel sensor is an actuator.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False\n"
+            "The inductive wheel sensor is a sensor"
+        ),
+    },
+    {
+        "question": ( #037
+            "What is (are) the task(s) of the inductive wheel sensor?\n\n"
+            "A. To measure the acceleration and deceleration of the wheel\n"
+            "B. To measure the wheel speed\n"
+            "C. To measure tire pressure\n"
+            "D. To measure force\n"
+            "E. A and B\n"
+            "F. B and C\n"
+            "G. C and D\n"
+            "H. All of the above\n"
+            "I. None of the above"
+        ),
+        "answer": (
+            "E. A and B"
+        ),
+    },
+    {
+        "question": ( #038
+            "When does a coil produce a voltage?\n\n"
+            "A. When there's no magnetic field\n"
+            "B. When there's a magnetic field\n"
+            "C. When there's a change in the magnetic field"
+        ),
+        "answer": (
+            "C. When there's a change in the magnetic field"
+        ),
+    },
+    {
+        "question": ( #039
+            "What does the magnitude of the self-induced voltage depend on?\n\n"
+            "A. The number of coil windings\n"
+            "B. The shape of the teeth\n"
+            "C. The strength of the magnet\n"
+            "D. The size of the air gap\n"
+            "E. A and D only\n"
+            "F. B and C only\n"
+            "G. All of the above\n"
+            "H. None of the above"
+        ),
+        "answer": (
+            "G. All of the above"
+        ),
+    },
+    {
+        "question": ( #040
+            "True or false: A magnetic resistance element (MRE) is a sensor.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #041
+            "What is the purpose of the magnetic resistance element (MRE) wheel sensor?\n\n"
+            "A. To measure force\n"
+            "B. To measure tire pressure\n"
+            "C. To measure the wheel speed\n"
+            "D. To measure the acceleration and deceleration of the wheel\n"
+            "E. A and B\n"
+            "F. C and D\n"
+            "G. All of the above\n"
+            "H. None of the above"
+        ),
+        "answer": (
+            "F. C and D"
+        ),
+    },
+    {
+        "question": ( #042
+            "What is the purpose of the high-pressure pump?\n\n"
+            "A. To provide the master cylinder with brake fluid\n"
+            "B. To pressurize the brake fluid when the driver brakes\n"
+            "C. To ensure that there is sufficient brake fluid pressure in the Electronic Stability Control (ESC)/Anti-Lock Braking System (ABS) when ESC/ABS is acting"
+        ),
+        "answer": (
+            "C. To ensure that there is sufficient brake fluid pressure in the Electronic Stability Control (ESC)/Anti-Lock Braking System (ABS) when ESC/ABS is acting"
+        ),
+    },
+]
