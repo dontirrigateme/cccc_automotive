2fba2b10536b4b7cef1dab3fa27f62f32ed19d17
dontirrigateme <smokey.alley44@gmail.com>
2026-06-16 04:31:59 -0400

    Add questions about tire and wheel properties

---
+++ electude_data/tires_and_wheels_properties.py
@@ -0,0 +1,382 @@
+# ELECTUDE - PROPERTIES OF TIRES AND WHEELS
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "A tire's sidewall is coded with the following: 195/60R 15 90T.\n"
+            "What does 195 indicate?\n\n"
+            "A. Rim diameter in inches\n"
+            "B. Load index\n"
+            "C. Tire height expressed as a percentage of the width\n"
+            "D. Tire width in millimeters"
+        ),
+        "answer": (
+            "D. Tire width in millimeters"
+        ),
+    },
+    {
+        "question": ( #002
+            "A tire's sidewall is coded with the following: 195/60R 15 90T.\n"
+            "What does 60 indicate?\n\n"
+            "A. Rim diameter in inches\n"
+            "B. Load index\n"
+            "C. Tire height expressed as a percentage of the width\n"
+            "D. Tire width in millimeters"
+        ),
+        "answer": (
+            "C. Tire height expressed as a percentage of the width"
+        ),
+    },
+    {
+        "question": ( #003
+            "A tire's sidewall is coded with the following: 195/60R 15 90T.\n"
+            "What does the R indicate?\n\n"
+            "A. That it is a radial tire\n"
+            "B. The speed rating\n"
+            "C. The load index\n"
+            "D. The manufacturer"
+        ),
+        "answer": (
+            "A. That it is a radial tire"
+        ),
+    },
+    {
+        "question": ( #004
+            "A tire's sidewall is coded with the following: 195/60R 15 90T.\n"
+            "What does 15 indicate?\n\n"
+            "A. Rim diameter in inches\n"
+            "B. Load index\n"
+            "C. Tire height expressed as a percentage of the width\n"
+            "D. Tire width in millimeters"
+        ),
+        "answer": (
+            "A. Rim diameter in inches"
+        ),
+    },
+    {
+        "question": ( #005
+            "A tire's sidewall is coded with the following: 195/60R 15 90T.\n"
+            "What does 90 indicate?\n\n"
+            "A. Rim diameter in inches\n"
+            "B. Load index\n"
+            "C. Tire height expressed as a percentage of the width\n"
+            "D. Tire width in millimeters"
+        ),
+        "answer": (
+            "B. Load index"
+        ),
+    },
+    {
+        "question": ( #006
+            "A tire's sidewall is coded with the following: 195/60R 15 90T.\n"
+            "What does the T indicate?\n\n"
+            "A. That it is an all-terrain tire\n"
+            "B. Manufacturer\n"
+            "C. Speed rating\n"
+            "D. Temperature"
+        ),
+        "answer": (
+            "C. Speed rating"
+        ),
+    },
+    {
+        "question": ( #007
+            "What is the distance from the centerline of the wheel to the road surface when the car is not moving called?\n\n"
+            "A. Static radius (r-stat)\n"
+            "B. Dynamic radius (r-dyn)\n"
+            "C. Transverse radius (r-tra)\n"
+            "D. Oblique radius (r-obl)"
+        ),
+        "answer": (
+            "A. Static radius (r-stat)"
+        ),
+    },
+    {
+        "question": ( #008
+            "What happens to the diameter of a tire when a car drives faster?\n\n"
+            "A. The diameter of the tire decreases\n"
+            "B. The diameter of the tire increases\n"
+            "C. The diameter of the tire stays the same"
+        ),
+        "answer": (
+            "B. The diameter of the tire increases"
+        ),
+    },
+    {
+        "question": ( #009
+            "What force makes the tire on a moving car expand?\n\n"
+            "A. Frictional force\n"
+            "B. Gravity\n"
+            "C. Centrifugal force"
+        ),
+        "answer": (
+            "C. Centrifugal force"
+        ),
+    },
+    {
+        "question": ( #010
+            "What is the distance from the centerline of the wheel to the road surface when the car is driving called?\n\n"
+            "A. Static radius (r-stat)\n"
+            "B. Dynamic radius (r-dyn)\n"
+            "C. Transverse radius (r-tra)\n"
+            "D. Oblique radius (r-obl)"
+        ),
+        "answer": (
+            "B. Dynamic radius (r-dyn)"
+        ),
+    },
+    {
+        "question": ( #011
+            "What does 'rolling circumference' mean?\n\n"
+            "A. This is the distance a tire travels in four revolutions\n"
+            "B. This is the distance a tire travels in one revolution\n"
+            "C. This is the distance the car travels after one crankshaft revolution"
+        ),
+        "answer": (
+            "B. This is the distance a tire travels in one revolution"
+        ),
+    },
+    {
+        "question": ( #012
+            "What changes when you install a tire that is a different size?\n\n"
+            "A. The diameter of the tire\n"
+            "B. The radius of the tire\n"
+            "C. The rolling circumference\n"
+            "D. All of the above"
+        ),
+        "answer": (
+            "D. All of the above"
+        ),
+    },
+    {
+        "question": ( #013
+            "When does longitudinal slip occur?\n\n"
+            "A. Only when the tire is being braked\n"
+            "B. Only when the tire is being driven\n"
+            "C. When driving and braking"
+        ),
+        "answer": (
+            "C. When driving and braking"
+        ),
+    },
+    {
+        "question": ( #014
+            "What can cause a lateral force?\n\n"
+            "A. Cornering maneuvers\n"
+            "B. A sloping road surface\n"
+            "C. Side winds\n"
+            "D. Driving downhill\n"
+            "E. A and B\n"
+            "F. A, B, and C\n"
+            "G. B and D\n"
+            "H. All of the above\n"
+            "I. None of the above"
+        ),
+        "answer": (
+            "F. A, B, and C"
+        ),
+    },
+    {
+        "question": ( #015
+            "What causes cornering force?\n\n"
+            "A. Lateral force\n"
+            "B. Tire lag\n"
+            "C. Slip angle\n"
+            "D. Longitudinal force"
+        ),
+        "answer": (
+            "A. Lateral force"
+        ),
+    },
+    {
+        "question": ( #016
+            "What force acts on the center of the wheel?\n\n"
+            "A. Cornering force\n"
+            "B. Longitudinal force\n"
+            "C. Lateral force"
+        ),
+        "answer": (
+            "C. Lateral force"
+        ),
+    },
+    {
+        "question": ( #017
+            "Where does the cornering force act?\n\n"
+            "A. The contact surface between the tire and the road surface\n"
+            "B. Between the drive shaft and the hub\n"
+            "C. The contact surface between the tire and the rim\n"
+            "D. The hub of the wheel"
+        ),
+        "answer": (
+            "A. The contact surface between the tire and the road surface"
+        ),
+    },
+    {
+        "question": ( #018
+            "What force acts on the point where tire deformation is greatest?\n\n"
+            "A. Lateral force\n"
+            "B. Longitudinal force\n"
+            "C. Cornering force"
+        ),
+        "answer": (
+            "C. Cornering force"
+        ),
+    },
+    {
+        "question": ( #019
+            "What is the distance called between the axis of rotation and the biggest deformation?\n\n"
+            "A. Slip angle\n"
+            "B. Rolling resistance\n"
+            "C. Adhesion force\n"
+            "D. Tire lag"
+        ),
+        "answer": (
+            "D. Tire lag"
+        ),
+    },
+    {
+        "question": ( #020
+            "What is the angle called between the rolling direction and the centerline of the tire?\n\n"
+            "A. Tire angle\n"
+            "B. Roll angle\n"
+            "C. Slip angle\n"
+            "D. Tracking angle"
+        ),
+        "answer": (
+            "C. Slip angle"
+        ),
+    },
+    {
+        "question": ( #021
+            "What does the slip angle depend on?\n\n"
+            "A. Tire pressure\n"
+            "B. Normal force on the wheel\n"
+            "C. Wheel position\n"
+            "D. Type of tire\n"
+            "E. All of the above\n"
+            "F. None of the above"
+        ),
+        "answer": (
+            "E. All of the above"
+        ),
+    },
+    {
+        "question": ( #022
+            "Where is the driving force transmitted?\n\n"
+            "A. Between the drive shaft and the rim\n"
+            "B. Between the rim and the tire\n"
+            "C. Between the differential and the drive shaft\n"
+            "D. Between the tire and the road surface"
+        ),
+        "answer": (
+            "D. Between the tire and the road surface"
+        ),
+    },
+    {
+        "question": ( #023
+            "What does the maximum transmitted driving force depend on?\n\n"
+            "A. The maximum braking force\n"
+            "B. The maximum longitudinal force\n"
+            "C. The maximum friction force"
+        ),
+        "answer": (
+            "C. The maximum fricion force"
+        ),
+    },
+    {
+        "question": ( #024
+            "True or false: The greater the coefficient of rolling resistance, the greater the rolling resistance.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #025
+            "What resisting force(s) can act on a vehicle?\n\n"
+            "A. Air resistance\n"
+            "B. Rolling resistance\n"
+            "C. Grade resistance\n"
+            "D. All of the above\n"
+            "E. None of the above"
+        ),
+        "answer": (
+            "D. All of the above"
+        ),
+    },
+    {
+        "question": ( #026
+            "True or false: The greater the coefficient of rolling resistance, the greater the normal force.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False"
+        ),
+    },
+    {
+        "question": ( #027
+            "True or false: The greater the normal force, the greater the rolling resistance.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #028
+            "A car is driving on a flat road.\n"
+            "What resistance(s) is (are) acting on the car?\n\n"
+            "A. Grade resistance\n"
+            "B. Air resistance\n"
+            "C. Rolling resistance\n"
+            "D. A and B\n"
+            "E. B and C\n"
+            "F. A and C\n"
+            "G. All of the above\n"
+            "H. None of the above"
+        ),
+        "answer": (
+            "E. B and C"
+        ),
+    },
+    {
+        "question": ( #029
+            "A car is driving up a slope.\n"
+            "What resistance(s) is (are) acting on the car?
+            "A. Grade resistance\n"
+            "B. Air resistance\n"
+            "C. Rolling resistance\n"
+            "D. All of the above\n"
+            "E. None of the above"
+        ),
+        "answer": (
+            "D. All of the above"
+        ),
+    },
+    {
+        "question": ( #030
+            "What happens if the driving force is smaller than the total resistance?\n\n"
+            "A. The car decelerates\n"
+            "B. The car continues at a constant speed\n"
+            "C. The car accelerates"
+        ),
+        "answer": (
+            "A. The car decelerates"
+        ),
+    },
+    {
+        "question": ( #031
+            "What happens if the driving force is greater than the total resistance?\n\n"
+            "A. The car decelerates\n"
+            "B. The car continues at a constant speed\n"
+            "C. The car accelerates"
+        ),
+        "answer": (
+            "C. The car accelerates"
+        ),
+    },
+]
