abeb253dbfc2ca0984aef2a50c72dfe2cdc2c53d
dontirrigateme <smokey.alley44@gmail.com>
2026-06-16 23:36:08 -0400

    Add tire theory questions and answers

---
+++ electude_data/tire_theory.py
@@ -0,0 +1,564 @@
+# ELECTUDE - TIRES THEORY
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "What is the main purpose of a tire?\n\n"
+            "A. To provide contact between the drive shaft and the tire\n"
+            "B. To provide contact between the tire and the wheel\n"
+            "C. To provide good contact between the vehicle chassis and the road surface"
+        ),
+        "answer": (
+            "C. To provide good contact between the vehicle chassis and the road surface"
+        ),
+    },
+    {
+        "question": ( #002
+            "What are the other purposes of a tire?\n\n"
+            "A. To provide improved suspension comfort\n"
+            "B. To transmit acceleration and braking forces\n"
+            "C. To carry the weight of the vehicle and its load\n"
+            "D. To transmit lateral forces\n"
+            "E. To provide good traction under various road and weather conditions\n"
+            "F. All of the above"
+        ),
+        "answer": (
+            "F. All of the above"
+        ),
+    },
+    {
+        "question": ( #003
+            "What is/are the purpose(s) of the tread?\n\n"
+            "A. To channel away water and dirt\n"
+            "B. To protect the carcass plies from damage\n"
+            "C. To transmit forces between the car and the road surface\n"
+            "D. A and B\n"
+            "E. B and C\n"
+            "F. A and C\n"
+            "G. All of the above"
+        ),
+        "answer": (
+            "F. A and C"
+        ),
+    },
+    {
+        "question": ( #004
+            "What is the purpose of the carcass plies?\n\n"
+            "A. To make the tread stiffer\n"
+            "B. To absorb forces that are transmitted to the tire\n"
+            "C. To channel away water and dirt\n"
+            "D. To connect the tread to the sidewall"
+        ),
+        "answer": (
+            "B. To absorb forces that are transmitted to the tire"
+        ),
+    },
+    {
+        "question": ( #005
+            "What type of carcass plies are shown in the picture?\n\n"
+            "A. Bias-ply (or) cross-ply tire\n"
+            "B. Radial tire"
+        ),
+        "answer": (
+            "B. Radial tire\n"
+            "The body plies run straight from bead to bead"
+        ),
+        "image": (
+            "images/carcass_plies01.png",
+        ),
+    },
+    {
+        "question": ( #006
+            "What type of carcass plies is this?\n\n"
+            "A. Bias-ply (or) cross-ply tire\n"
+            "B. Radial tire"
+        ),
+        "answer": (
+            "A. Bias-ply (or) cross-ply tire\n"
+            "The body plies are arranged diagonally across each other"
+        ),
+        "image": (
+            "images/carcass_plies02.png",
+        ),
+    },
+    {
+        "question": ( #007
+            "What is the purpose of the sidewall?\n\n"
+            "A. To protect the carcass plies from damage\n"
+            "B. To channel away water and dirt\n"
+            "C. To connect the tread to the sidewall\n"
+            "D. To make the tread stiffer"
+        ),
+        "answer": (
+            "A. To protect the carcass plies from damage"
+        ),
+    },
+    {
+        "question": ( #008
+            "What is the purpose of the shoulder?\n\n"
+            "A. To make the tread stiffer\n"
+            "B. To protect the carcass plies from damage\n"
+            "C. To channel away water and dirt\n"
+            "D. To connect the tread to the sidewall"
+        ),
+        "answer": (
+            "D. To connect the tread to the sidewall"
+        ),
+    },
+    {
+        "question": ( #009
+            "What is the purpose of the bead?\n\n"
+            "A. To mount and seal the tire against the rim\n"
+            "B. To channel away water and dirt\n"
+            "C. To cool down the tire\n"
+            "D. To make the tread stiffer"
+        ),
+        "answer": (
+            "A. To mount and seal the tire against the rim"
+        ),
+    },
+    {
+        "question": ( #010
+            "What is the purpose of the belt?\n\n"
+            "A. To cool down the tire\n"
+            "B. To transmit forces between the car and the road surface\n"
+            "C. To make the tread stiffer\n"
+            "D. To connect the tread to the sidewall"
+        ),
+        "answer": (
+            "C. To make the tread stiffer"
+        ),
+    },
+    {
+        "question": ( #011
+            "Which forces deform the tire when pulling away?\n\n"
+            "A. Braking forces\n"
+            "B. Steering forces\n"
+            "C. Acceleration forces\n"
+            "D. Tracking forces"
+        ),
+        "answer": (
+            "C. Acceleration forces"
+        ),
+    },
+    {
+        "question": ( #012
+            "In which direction does the tire deform when pulling away?\n\n"
+            "A. Vertical direction\n"
+            "B. Transverse direction\n"
+            "C. Longitudinal direction"
+        ),
+        "answer": (
+            "C. Longitudinal direction"
+        ),
+    },
+    {
+        "question": ( #013
+            "Which forces deform the tire when braking?\n\n"
+            "A. Lateral forces\n"
+            "B. Tracking forces\n"
+            "C. Braking forces\n"
+            "D. Acceleration forces"
+        ),
+        "answer": (
+            "C. Braking forces"
+        ),
+    },
+    {
+        "question": ( #014
+            "In which direction does the tire deform when braking?\n\n"
+            "A. Longitudinal direction\n"
+            "B. Transverse direction\n"
+            "C. Vertical direction"
+        ),
+        "answer": (
+            "A. Longitudinal direction"
+        ),
+    },
+    {
+        "question": ( #015
+            "A car is driving in a roundabout.\n"
+            "Which are the main forces that deform the tire?\n\n"
+            "A. Braking forces\n"
+            "B. Tracking forces\n"
+            "C. Acceleration forces"
+        ),
+        "answer": (
+            "B. Tracking forces"
+        ),
+    },
+    {
+        "question": ( #016
+            "A car is driving in a roundabout.\n"
+            "In which direction does the tire deform?\n\n"
+            "A. Vertical direction\n"
+            "B. Longitudinal direction\n"
+            "C. Transverse direction"
+        ),
+        "answer": (
+            "C. Transverse direction"
+        ),
+    },
+    {
+        "question": ( #017
+            "What is a lateral tread profile particularly suited for?\n\n"
+            "A. To transmit forces in the driving direction\n"
+            "B. To transmit forces laterally"
+        ),
+        "answer": (
+            "A. To transmit forces in the driving direction"
+        ),
+    },
+    {
+        "question": ( #018
+            "Which has the most grip when cornering?\n\n"
+            "A. Longitudinal tread profile\n"
+            "B. Combined tread profile\n"
+            "C. Lateral tread profile"
+        ),
+        "answer": (
+            "A. Longitudinal tread profile"
+        ),
+    },
+    {
+        "question": ( #019
+            "What is a longitudinal tread profile particularly suited for?\n\n"
+            "A. To transmit forces laterally\n"
+            "B. To transmit forces in the driving direction"
+        ),
+        "answer": (
+            "A. To transmit forces laterally"
+        ),
+    },
+    {
+        "question": ( #020
+            "Which tread profile is the most common?\n\n"
+            "A. Combined tread profile\n"
+            "B. Lateral tread profile\n"
+            "C. Longitudinal tread profile"
+        ),
+        "answer": (
+            "A. Combined tread profile"
+        ),
+    },
+    {
+        "question": ( #021
+            "What is the MS tire tread profile suitable for?\n\n"
+            "A. Compacted snow and/or ice\n"
+            "B. Mud and snow\n"
+            "C. Loose dirt\n"
+            "D. All of the above"
+        ),
+        "answer": (
+            "B. Mud and snow"
+        ),
+    },
+    {
+        "question": ( #022
+            "What economic demand(s) is/are placed on a tire?\n\n"
+            "A. Low rolling resistance\n"
+            "B. Long service life\n"
+            "C. Good price\n"
+            "D. Good grip to the road surface\n"
+            "E. A and D\n"
+            "F. B and D\n"
+            "G. C and D\n"
+            "H. A, B, and C\n"
+            "I. B, C, and D\n"
+            "J. All of the above"
+        ),
+        "answer": (
+            "H. A, B, and C"
+        ),
+    },
+    {
+        "question": ( #023
+            "What environmental demand(s) is/are placed on a tire?\n\n"
+            "A. Good suspension comfort\n"
+            "B. Low level of noise production\n"
+            "C. Suitable for reuse\n"
+            "D. A and B\n"
+            "E. A and C\n"
+            "F. B and C\n"
+            "G. All of the above"
+        ),
+        "answer": (
+            "F. B and C"
+        ),
+    },
+    {
+        "question": ( #024
+            "What safety demand(s) is/are placed on a tire?\n\n"
+            "A. Low level of noise production\n"
+            "B. Good grip to the road surface\n"
+            "C. Good suspension comfort\n"
+            "D. Damage resistance\n"
+            "E. A, B, and C\n"
+            "F. A and B\n"
+            "G. B and D\n"
+            "H. C and D\n"
+            "I. All of the above"
+        ),
+        "answer": (
+            "G. B and D"
+        ),
+    },
+    {
+        "question": ( #025
+            "What comfort demands are placed on a tire?\n\n"
+            "A. Low level of noise production\n"
+            "B. Low rolling resistance\n"
+            "C. Good suspension comfort\n"
+            "D. Long service life\n"
+            "E. A, B, and D\n"
+            "F. B, C, and D\n"
+            "G. A and C\n"
+            "H. B and D\n"
+            "I. All of the above"
+        ),
+        "answer": (
+            "G. A and C"
+        ),
+    },
+    {
+        "question": ( #026
+            "Which statement is true?\n\n"
+            "A. Tube-type tires are common\n"
+            "B. A tube-type tire has an inner tube\n"
+            "C. A tube-type tire needs an airtight inner liner\n"
+            "D. All of the above"
+        ),
+        "answer": (
+            "B. A tube-type tire has an inner tube"
+        ),
+    },
+    {
+        "question": ( #027
+            "Which statement(s) is/are true?\n\n"
+            "A. Tubeless tires are common\n"
+            "B. A tubeless tire needs an airtight inner liner\n"
+            "C. Tubeless tires do not have an inflatable inner tube\n"
+            "D. A and C only\n"
+            "E. A and B only\n"
+            "F. B and C only\n"
+            "G. All of the above"
+        ),
+        "answer": (
+            "G. All of the above"
+        ),
+    },
+    {
+        "question": ( #028
+            "Does a tire with little tread have grip in wet weather?\n\n"
+            "A. Yes, the tire has a lot of grip\n"
+            "B. No, the tire has little grip"
+        ),
+        "answer": (
+            "B. No, the tire has little grip"
+        ),
+    },
+    {
+        "question": ( #029
+            "What can you use to check the tread depth?\n\n"
+            "A. Tire pressure gauge\n"
+            "B. Wear indicator\n"
+            "C. Tread depth gauge\n"
+            "D. Measuring rule\n"
+            "E. A and B only\n"
+            "F. B and C only\n"
+            "G. C and D only\n"
+            "H. All of the above"
+        ),
+        "answer": (
+            "F. B and C only"
+        ),
+    },
+    {
+        "question": ( #030
+            "Where can you find a wear indicator?\n\n"
+            "A. On the rim\n"
+            "B. On the tread\n"
+            "C. In the main grooves\n"
+            "D. On the side of the tire"
+        ),
+        "answer": (
+            "C. In the main grooves"
+        ),
+    },
+    {
+        "question": ( #031
+            "When does a wear indicator show that a tire requires replacement?\n\n"
+            "A. When the indicator is below the level of the tread\n"
+            "B. When the indicator can no longer be seen\n"
+            "C. When the indicator is level with the tread surface"
+        ),
+        "answer": (
+            "C. When the indicator is level with the tread surface"
+        ),
+    },
+    {
+        "question": ( #032
+            "Why can you not continue to drive with a flat tire?\n\n"
+            "A. This damages the transmission\n"
+            "B. You might damage the tire and wheel\n"
+            "C. This is not good for the engine\n"
+            "D. The car is difficult to steer\n"
+            "E. B and D only\n"
+            "F. A and C only\n"
+            "G. A, B, and D only\n"
+            "H. All of the above"
+        ),
+        "answer": (
+            "E. B and D only"
+        ),
+    },
+    {
+        "question": ( #033
+            "A punctured tire has been treated with a tire repair kit, but the pressure remains below 1.8 bar.\n"
+            "What is causing this?\n\n"
+            "A. The tire is too cold to be able to achieve the correct pressure\n"
+            "B. The puncture is too large to seal with the tire repair kit\n"
+            "C. The wrong tire has been fitted"
+        ),
+        "answer": (
+            "B. The puncture is too large to seal with the tire repair kit"
+        ),
+    },
+    {
+        "question": ( #034
+            "There are tores which allow you to continue driving for a certain number of kilometers without carrying out a repair if you have a puncture.\n"
+            "What are these tires called?\n\n"
+            "A. Intertires\n"
+            "B. Run-flat tires\n"
+            "C. Rain/Med tires\n"
+            "D. Emergency tires"
+        ),
+        "answer": (
+            "B. Run-flat tires"
+        ),
+    },
+    {
+        "question": ( #035
+            "What types of run-flat tire are there?\n\n"
+            "A. Run-flat tires with reinforced sidewalls\n"
+            "B. Run-flat tires with a support ring\n"
+            "C. Run-flat tires with reinforced tread\n"
+            "D. A and B only\n"
+            "E. A and C only\n"
+            "F. B and C only\n"
+            "G. All of the above"
+        ),
+        "answer": (
+            "D. A and B only"
+        ),
+    },
+    {
+        "question": ( #036
+            "What is the maximum speed youo may drive with a punctured run-flat tire?\n\n"
+            "A. 60 kph\n"
+            "B. 70 kph\n"
+            "C. 80 kph\n"
+            "D. 90 kph"
+        ),
+        "answer": (
+            "C. 80 kph"
+        ),
+    },
+    {
+        "question": ( #037
+            "What could cause damage to the sidewall?\n\n"
+            "A. Driving/Scrubbing against a curb\n"
+            "B. Excessive tire load\n"
+            "C. A locked wheel"
+        ),
+        "answer": (
+            "A. Driving/Scrubbing against a curb"
+        ),
+    },
+    {
+        "question": ( #038
+            "The carcass plies can become damaged if you drive against a curb.\n"
+            "What else can damage the carcass?\n\n"
+            "A. A locked wheel\n"
+            "B. Tire-fitting error\n"
+            "C. Driving for a long time with a flat tire\n"
+            "D. A and B only\n"
+            "E. A and C only\n"
+            "F. B and C only\n"
+            "G. All of the above"
+        ),
+        "answer": (
+            "F. B and C only"
+        ),
+    },
+    {
+        "question": ( #039
+            "What is a possible cause for damage to the tire bead?\n\n"
+            "A. A locked wheel\n"
+            "B. Scrubbing the tire against a curb\n"
+            "C. Tire-fitting error"
+        ),
+        "answer": (
+            "C. Tire-fitting error"
+        ),
+    },
+    {
+        "question": ( #040
+            "What are the consequences of over-inflation?\n\n"
+            "A. High rolling resistance\n"
+            "B. Premature wear in the center of the tread\n"
+            "C. Poor suspension comfort\n"
+            "D. Tire overheating\n"
+            "E. A and B only\n"
+            "F. B and C only\n"
+            "G. C and D only\n"
+            "H. A, B, and C only\n"
+            "I. B, C, and D only\n"
+            "J. All of the above"
+        ),
+        "answer": (
+            "F. B and C only"
+        ),
+    },
+    {
+        "question": ( #041
+            "What are the consequences of under-inflation?\n\n"
+            "A. High rolling resistance\n"
+            "B. Tire overheating\n"
+            "C. Premature wear in the center of the tread\n"
+            "D. Premature wear on the outside of the tread\n"
+            "E. A, B, and C only\n"
+            "F. B, C, and D only\n"
+            "G. A, C, and D only\n"
+            "H. A, B, and D only\n"
+            "I. All of the above"
+        ),
+        "answer": (
+            "H. A, B, and D only"
+        ),
+    },
+    {
+        "question": ( #042
+            "\n\n"
+            "A. \n"
+            "B. \n"
+            "C. \n"
+            "D. "
+        ),
+        "answer": (
+            ""
+        ),
+    },
+    {
+        "question": ( #043
+            "\n\n"
+            "A. \n"
+            "B. \n"
+            "C. \n"
+            "D. "
+        ),
+        "answer": (
+            ""
+        ),
+    },
+]
