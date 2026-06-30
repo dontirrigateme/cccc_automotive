5e7a666b1b559ed277382d732522696ab5f70e01
dontirrigateme <smokey.alley44@gmail.com>
2026-06-30 03:41:04 -0400

    Add wheels theory questions and answers
    Added a comprehensive set of questions and answers related to wheels theory, including various types of wheels, their advantages, and specifications.

---
+++ electude_data/wheels_theory.py
@@ -0,0 +1,322 @@
+# ELECTUDE - WHEELS THEORY
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "Which wheel is the alloy wheel?\n\n"
+            "A.\n"
+            "B.\n"
+            "C."
+        ),
+        "answer": (
+            "B."
+        ),
+        "image": (
+            "images/wheel_rims01.png",
+        ),
+    },
+    {
+        "question": ( #002
+            "Which wheel is the carbon fiber wheel?\n\n"
+            "A.\n"
+            "B.\n"
+            "C."
+        ),
+        "answer": (
+            "C."
+        ),
+        "image": (
+            "images/wheel_rims01.png",
+        ),
+    },
+    {
+        "question": ( #003
+            "Which wheel is the steel wheel?\n\n"
+            "A.\n"
+            "B.\n"
+            "C."
+        ),
+        "answer": (
+            "A."
+        ),
+        "image": (
+            "images/wheel_rims01.png",
+        ),
+    },
+    {
+        "question": ( #004
+            "What are the advantages of an alloy wheel compared to a steel wheel?\n\n"
+            "A. Stronger and less expensive\n"
+            "B. Less weight and truly round\n"
+            "C. All of the above\n"
+            "D. No advantages"
+        ),
+        "answer": (
+            "B. Less weight and truly round"
+        ),
+    },
+    {
+        "question": ( #005
+            "What are some characteristics of a carbon fiber wheel?\n\n"
+            "A. Easy to repair\n"
+            "B. Expensive\n"
+            "C. Very light weight\n"
+            "D. A and B\n"
+            "E. B and C\n"
+            "F. A and C"
+        ),
+        "answer": (
+            "E. B and C"
+        ),
+    },
+    {
+        "question": ( #006
+            "What are the advantages of a steel wheel compared to an alloy wheel?\n\n"
+            "A. Easier to repair and less expensive\n"
+            "B. Truly round and less weight\n"
+            "C. All of the above\n"
+            "D. No advantages"
+        ),
+        "answer": (
+            "A. Easier to repair and less expensive"
+        ),
+    },
+    {
+        "question": ( #007
+            "What is indicated with the letter A?\n\n"
+            "A. Drop center\n"
+            "B. Retaining hump\n"
+            "C. Wheel flange\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "C. Wheel flange"
+        ),
+        "image": (
+            "images/drop_center_wheels01.png",
+        ),
+    },
+    {
+        "question": ( #008
+            "What is indicated with the letter B?\n\n"
+            "A. Drop center\n"
+            "B. Retaining hump\n"
+            "C. Wheel flange\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "B. Retaining hump"
+        ),
+        "image": (
+            "images/drop_center_wheels01.png",
+        ),
+    },
+    {
+        "question": ( #009
+            "What is indicated with the letter C?\n\n"
+            "A. Drop center\n"
+            "B. Retaining hump\n"
+            "C. Wheel flange\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "A. Drop center"
+        ),
+        "image": (
+            "images/drop_center_wheels01.png",
+        ),
+    },
+    {
+        "question": ( #010
+            "What is the purpose of the retaining hump?\n\n"
+            "A. To ensure that the tire slides into the drop center when driving straight ahead\n"
+            "B. To ensure that the tire does not slide into the drop center when cornering\n"
+            "C. To ensure that the tire slides into the drop center when cornering"
+        ),
+        "answer": (
+            "B. To ensure that the tire does not slide into the drop center when cornering"
+        ),
+    },
+    {
+        "question": ( #011
+            "What does rim offset mean?\n\n"
+            "A. This is the distance from the center hole to the road surface\n"
+            "B. This is the offset of the wheel in relation to the fender\n"
+            "C. The distance from the center line to the hub-mounting surface\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "C. The distance from the center line to the hub-mounting surface"
+        ),
+    },
+    {
+        "question": ( #012
+            "What is the advantage of positive offset?\n\n"
+            "A. There is more room for the brake drum or brake disc\n"
+            "B. Cheaper to manufacture\n"
+            "C. Lighter than negative offset"
+        ),
+        "answer": (
+            "A. There is more room for the brake drum or brake disc"
+        ),
+    },
+    {
+        "question": ( #013
+            "A wheel has the following code:\n\n"
+            "5 1/2 J x 14 ET10 4x140\n\n"
+            "Which element is the pitch circle?/n/n"
+            "A. 5 1/2\n"
+            "B. J\n"
+            "C. 14\n"
+            "D. ET10\n"
+            "E. 4x140"
+        ),
+        "answer": (
+            "E. 4x140"
+        ),
+    },
+    {
+        "question": ( #014
+            "A wheel has the following code:\n\n"
+            "5 1/2 J x 14 ET10 4x140\n\n"
+            "Which element is the diameter of the wheel?\n\n"
+            "A. 5 1/2\n"
+            "B. J\n"
+            "C. 14\n"
+            "D. ET10\n"
+            "E. 4x140"
+        ),
+        "answer": (
+            "C. 14"
+        ),
+    },
+    {
+        "question": ( #015
+            "A wheel has the following code:\n\n"
+            "5 1/2 J x 14 ET10 4x140\n\n"
+            "Which element is the width of the wheel?\n\n"
+            "A. 5 1/2\n"
+            "B. J\n"
+            "C. 14\n"
+            "D. ET10\n"
+            "E. 4x140"
+        ),
+        "answer": (
+            "A. 5 1/2"
+        ),
+    },
+    {
+        "question": ( #016
+            "A wheel has the following code:\n\n"
+            "5 1/2 J x 14 ET10 4x140\n\n"
+            "Which element is the bead profile code?\n\n"
+            "A. 5 1/2\n"
+            "B. J\n"
+            "C. 14\n"
+            "D. ET10\n"
+            "E. 4x140"
+        ),
+        "answer": (
+            "B. J"
+        ),
+    },
+    {
+        "question": ( #017
+            "A wheel has the following code:\n\n"
+            "5 1/2 J x 14 ET10 4x140\n\n"
+            "Which element is the offset?\n\n"
+            "A. 5 1/2\n"
+            "B. J\n"
+            "C. 14\n"
+            "D. ET10\n"
+            "E. 4x140"
+        ),
+        "answer": (
+            "D. ET10"
+        ),
+    },
+    {
+        "question": ( #018
+            "On the 4-hole wheel, what size does the pitch circle indicate?\n\n"
+            "A. Size D\n"
+            "B. Size A\n"
+            "C. Size B\n"
+            "D. Size C"
+        ),
+        "answer": (
+            "A. Size D"
+        ),
+        "image": (
+            "images/pitch_circle01.png",
+        ),
+    },
+    {
+        "question": ( #019
+            "On the 5-hole wheel, what size does the pitch circle indicate?\n\n"
+            "A. Size B\n"
+            "B. Size D\n"
+            "C. Size A\n"
+            "D. Size C"
+        ),
+        "answer": (
+            "D. Size C"
+        ),
+        "image": (
+            "images/pitch_circle02.png",
+        ),
+    },
+    {
+        "question": ( #019
+            "On what rim widths can a 135 R 15 tire be fitted?\n\n"
+            "A. 3 1/2\n"
+            "B. 4 1/2\n"
+            "C. 4\n"
+            "D. A and D\n"
+            "E. B and D\n"
+            "F. All of the above\n"
+            "G. None of the above"
+        ),
+        "answer": (
+            "F. All of the above"
+        ),
+        "image": (
+            "images/wheel_tire_size_chart01.png",
+        ),
+    },
+    {
+        "question": ( #020
+            "On which rim widths can a 165/70 R 13 tire be fitted?\n\n"
+            "A. 5 1/2\n"
+            "B. 6\n"
+            "C. 4 1/2\n"
+            "D. 5\n"
+            "E. A, B, and C\n"
+            "F. B, C, and D\n"
+            "G. A, B, and D\n"
+            "H. A, C, and D\n"
+            "I. All of the above"
+        ),
+        "answer": (
+            "H. A, C, and D"
+        ),
+        "image": (
+            "images/wheel_tire_size_chart01.png",
+        ),
+    },
+    {
+        "question": ( #021
+            "A customer wants to replace a 155 R 13 with a tire from the 70 series.\n\n"
+            "The rim width is 4 1/2.\n\n"
+            "Which tire would you choose?\n\n"
+            "A. 175/70 R 14\n"
+            "B. 175/70 R 13\n"
+            "C. 155/70 R 13"
+        ),
+        "answer": (
+            "B. 175/70 R 13"
+        ),
+        "image": (
+            "images/wheel_tire_size_chart01.png",
+        ),
+    },
+]
