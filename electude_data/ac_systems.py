c808a2389a856c077e94a5799e05ddfa663ff1e7
dontirrigateme <smokey.alley44@gmail.com>
2026-07-05 02:37:17 -0400

    Add air-conditioning systems questions and answers

---
+++ electude_data/ac_systems.py
@@ -0,0 +1,567 @@
+# ELECTUDE - AIR-CONDITIONING SYSTEMS
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "What can be achieved with a climate-control system?\n\n"
+            "A. A comfortable humidity inside the vehicle\n"
+            "B. A comfortable climate inside the vehicle\n"
+            "C. A comfortable temperature inside the vehicle\n"
+            "D. All of the above"
+        ),
+        "answer": (
+            "D. All of the above"
+        ),
+    },
+    {
+        "question": ( #002
+            "What is the heater used for?\n\n"
+            "A. To raise the temperature inside the vehicle\n"
+            "B. To raise the humidity inside the vehicle\n"
+            "C. To lower the humidity inside the vehicle\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "A. To raise the temperature inside the vehicle"
+        ),
+    },
+    {
+        "question": ( #003
+            "The air conditioning cools the incoming air.\n"
+            "What else does the air conditioning do to the incoming air?\n\n"
+            "A. The air conditioning lowers the humidity\n"
+            "B. The air conditioning blows the air into the passenger compartment\n"
+            "C. The air conditioning raises the humidity\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "A. The air conditioning lowers the humidity"
+        ),
+    },
+    {
+        "question": ( #004
+            "How can you distribute warm or cool air through the interior?\n\n"
+            "A. By switching on the air conditioning\n"
+            "B. By switching on the heater\n"
+            "C. By switching on the fan\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "C. By switching on the fan"
+        ),
+    },
+    {
+        "question": ( #005
+            "What is needed to raise the temperature of a substance?\n\n"
+            "A. Energy in mechanical form\n"
+            "B. Energy in the form of light\n"
+            "C. Energy in the form of heat\n"
+            "D. All of the above"
+        ),
+        "answer": (
+            "C. Energy in the form of heat"
+        ),
+    },
+    {
+        "question": ( #006
+            "What can you say about the transfer of heat?\n\n"
+            "A. Heat moves from cold to hot\n"
+            "B. Heat moves from both hot to cold, and from cold to hot\n"
+            "C. Heat moves from hot to cold\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "C. Heat moves from hot to cold"
+        ),
+    },
+    {
+        "question": ( #007
+            "Why does warm air rise?\n\n"
+            "A. Warm air is lighter than cold air\n"
+            "B. Warm air is heavier than cold air\n"
+            "C. Warm air mixes with cold air\n"
+            "D. Warm air moves slower than cold air"
+        ),
+        "answer": (
+            "A. Warm air is lighter than cold air"
+        ),
+    },
+    {
+        "question": ( #008
+            "What happens to air that cools?\n\n"
+            "A. The air becomes heavier\n"
+            "B. The air stays the samw weight\n"
+            "C. The air becomes lighter\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "A. The air becomes heavier"
+        ),
+    },
+    {
+        "question": ( #009
+            "What are the different types of heat transfer?\n\n"
+            "A. Convection\n"
+            "B. Radiation\n"
+            "C. Conduction\n"
+            "D. All of the above"
+        ),
+        "answer": (
+            "D. All of the above"
+        ),
+    },
+    {
+        "question": ( #010
+            "Heat energy that causes a change in temperature is called:\n\n"
+            "A. Dynamic heat\n"
+            "B. Measurable heat\n"
+            "C. Congruent heat\n"
+            "D. Latent heat"
+        ),
+        "answer": (
+            "B. Measurable heat"
+        ),
+    },
+    {
+        "question": ( #011
+            "Heat energy that causes a change in state is called:\n\n"
+            "A. Dynamic heat\n"
+            "B. Measurable heat\n"
+            "C. Congruent heat\n"
+            "D. Latent heat"
+        ),
+        "answer": (
+            "D. Latent heat"
+        ),
+    },
+    {
+        "question": ( #012
+            "A change in state uses more energy than a change in temperature.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #013
+            "A change of state in one direction uses more energy than changing state in the opposite direction (e.g., condensation uses more energy than evaporation).\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False\n"
+            "A change of state in either direction uses an equal amount of energy (e.g., condensation uses just as much energy as evaporation)."
+        ),
+    },
+    {
+        "question": ( #014
+            "What does it mean if a substance is superheated?\n\n"
+            "A. It means the substance is the hottest it can possibly be\n"
+            "B. It means it was heated rapidly\n"
+            "C. It means that the temperature of the substance is above the boiling point\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "C. It means that the temperature of the substance is above the boiling point"
+        ),
+    },
+    {
+        "question": ( #015
+            "What happens if you add more energy to a superheated gas?\n\n"
+            "A. The gas becomes even more gaseous\n"
+            "B. The gas starts to condense again\n"
+            "C. The temperature of the gas increases\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "C. The temperatyre of the gas increases"
+        ),
+    },
+    {
+        "question": ( #016
+            "The temperature below the boiling point is called subcooled.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #017
+            "What do you know for sure when water is subcooled?\n\n"
+            "A. The water is completely liquid\n"
+            "B. The water is completely changed into ice\n"
+            "C. The water is completely changed into steam\n"
+            "D. The water is evaporating into steam"
+        ),
+        "answer": (
+            "A. The water is completely liquid"
+        ),
+    },
+    {
+        "question": ( #018
+            "What can you say with certainty when steam is superheated?\n\n"
+            "A. In the steam, there is no more liquid present\n"
+            "B. In the steam, there is no more gas present\n"
+            "C. The steam pressure must be higher than 100 kPa\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "A. In the steam, there is no more liquid present"
+        ),
+    },
+    {
+        "question": ( #019
+            "As pressure increases, the boiling point decreases.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False\n"
+            "As pressure increases, the boiling point increases"
+        ),
+    },
+    {
+        "question": ( #020
+            "As pressure decreases, the boiling point decreases\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #021
+            "What effect does a higher ambient pressure have on the boiling point?\n\n"
+            "A. The boiling point remains the same\n"
+            "B. The boiling point goes up\n"
+            "C. The boiling point goes down"
+        ),
+        "answer": (
+            "B. The boiling point goes up"
+        ),
+    },
+    {
+        "question": ( #022
+            "What effect does a lower ambient pressure have on the boiling point?\n\n"
+            "A. The boiling point remains the same\n"
+            "B. The boiling point goes up\n"
+            "C. The boiling point goes down"
+        ),
+        "answer": (
+            "C. The boiling point goes down"
+        ),
+    },
+    {
+        "question": ( #023
+            "Refrigerant has a high boiling point.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False\n"
+            "Refrigerant has a low boiling point"
+        ),
+    },
+    {
+        "question": ( #024
+            "View the substances in the cylinders.\n"
+            "In what state is the water (H2O) at 20°C?\n\n"
+            "A. Gaseous state\n"
+            "B. Liquid state\n"
+            "C. Solid state"
+        ),
+        "answer": (
+            "B. Liquid state"
+        ),
+        "image": (
+            "images/water_and_refrigerant01.png",
+        ),
+    },
+    {
+        "question": ( #025
+            "In what state is the refrigerant (R134a) at 20°C?\n\n"
+            "A. Gaseous state\n"
+            "B. Liquid state\n"
+            "C. Solid state"
+        ),
+        "answer": (
+            "A. Gaseous state"
+        ),
+        "image": (
+            "images/water_and_refrigerant01.png",
+        ),
+    },
+    {
+        "question": ( #026
+            "At 20°C, the refrigerant (R134a) is in a gaseous state.\n"
+            "What happens to the refrigerant during a temperature drop from 20°C to -2°C?\n\n"
+            "A. The refrigerant has become liquid\n"
+            "B. The refrigerant has changed into a solid substance\n"
+            "C. The refrigerant remains gaseous"
+        ),
+        "answer": (
+            "C. The refrigerant remains gaseous"
+        ),
+        "image": (
+            "images/water_and_refrigerant02.png",
+        ),
+    },
+    {
+        "question": ( #027
+            "The refrigerant (R134a) is liquid when it's colder than -26°C.\n"
+            "What does this mean for the boiling point of refrigerant?\n\n"
+            "A. Refrigerant boils at a temperature above -26°C\n"
+            "B. Refrigerant boils at a temperature below -26°C\n"
+            "C. Refrigerant boils at a temperature above -100°C"
+        ),
+        "answer": (
+            "A. Refrigerant boils at a temperature above -26°C"
+        ),
+        "image": (
+            "images/water_and_refrigerant03.png",
+        ),
+    },
+    {
+        "question": ( #028
+            "What is the refrigerant term for carbon dioxide?\n\n"
+            "A. R134a\n"
+            "B. R744\n"
+            "C. R1234yf\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "B. R744"
+        ),
+    },
+    {
+        "question": ( #029
+            "Refrigerant reaches the compressor as a gas, and the temperature and pressure are low.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #030
+            "During compression, the pressure decreases, causing the temperature and the boiling point of the refrigerant to drop.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False\n"
+            "During compression, the pressure increases, causing the temperature and the boiling point of the refrigerant to rise."
+        ),
+    },
+    {
+        "question": ( #031
+            "How does refrigerant leave the compressor?\n\n"
+            "A. Liquid\n"
+            "B. Solid\n"
+            "C. Gas\n"
+            "D. None of the above"
+        ),
+        "answer": (
+            "C. Gas"
+        ),
+    },
+    {
+        "question": ( #032
+            "What happens when the refrigerant flows through the condenser?\n\n"
+            "A. The pressure of the refrigerant remains the same\n"
+            "B. The temperature of the refrigerant drops\n"
+            "C. Both A and B\n"
+            "D. Neither A nor B"
+        ),
+        "answer": (
+            "C. Both A and B"
+        ),
+    },
+    {
+        "question": ( #033
+            "In what state is the refrigerant when it leaves the condenser?\n\n"
+            "A. Liquid\n"
+            "B. Gaseous"
+        ),
+        "answer": (
+            "A. Liquid"
+        ),
+    },
+    {
+        "question": ( #034
+            "In what state is the refrigerant when it reaches the orifice/restriction device?\n\n"
+            "A. Fully gaseous\n"
+            "B. Fully liquid\n"
+            "C. High pressure\n"
+            "D. Low pressure\n"
+            "E. High temperature\n"
+            "F. Low temperature\n"
+            "G. B, C, and E\n"
+            "H. B, C, and E\n"
+            "I. A, D, and F"
+        ),
+        "answer": (
+            "H. B, C, and E"
+        ),
+    },
+    {
+        "question": ( #035
+            "What happens when the refrigerant flows through the orifice/restriction device?\n\n"
+            "A. The boiling point decreases\n"
+            "B. The pressure drops\n"
+            "C. The temperature drops\n"
+            "D. All of the above"
+        ),
+        "answer": (
+            "D. All of the above"
+        ),
+    },
+    {
+        "question": ( #036
+            "In what state does the refrigerant reach the evaporator?\n\n"
+            "A. Low temperature\n"
+            "B. Low pressure\n"
+            "C. Low boiling point\n"
+            "D. All of the above"
+        ),
+        "answer": (
+            "D. All of the above"
+        ),
+    },
+    {
+        "question": ( #037
+            "Refrigerant contributes to the greenhouse effect.\n"
+            "What is the consequence of the greenhouse effect?\n\n"
+            "A. A falling sea level\n"
+            "B. Global cooling\n"
+            "C. Global warming\n"
+            "D. a lower humidity on earth"
+        ),
+        "answer": (
+            "C. Global warming"
+        ),
+    },
+    {
+        "question": ( #038
+            "Which classification to refrigerant gases fall under?\n\n"
+            "A. Ground gas\n"
+            "B. Ozone gas\n"
+            "C. Exhaust gas\n"
+            "D. Greenhouse gas"
+        ),
+        "answer": (
+            "D. Greenhouse gas"
+        ),
+    },
+    {
+        "question": ( #039
+            "What does the abbreviation GWP stand for?\n\n"
+            "A. Global Warming Potential\n"
+            "B. Global Warming Perspective\n"
+            "C. Global Warming Point"
+        ),
+        "answer": (
+            "A. Global Warming Potential"
+        ),
+    },
+    {
+        "question": ( #040
+            "What does it mean if the GWP of a of a particular type of refrigerant is high?\n\n"
+            "A. That the refrigerant contributes little to the greenhouse effect\n"
+            "B. That the refrigerant contributes significantly to the greenhouse effect\n"
+            "C. That this type of refrigerant reduces the greenhouse effect"
+        ),
+        "answer": (
+            "B. That the refrigerant contributes significantly to the greenhouse effect"
+        ),
+    },
+    {
+        "question": ( #041
+            "What type of refrigerant contributes the most to the greenhouse effect?\n\n"
+            "A. R744\n"
+            "B. R1234yf\n"
+            "C. R134a"
+        ),
+        "answer": (
+            "C. R134a"
+        ),
+        "image": (
+            "images/refrigerants01.png",
+        ),
+    },
+    {
+        "question": ( #042
+            "Which air-conditioning component is centrally-located at the front of the engine compartment?\n\n"
+            "A. Flow-control valve\n"
+            "B. Compressor\n"
+            "C. Evaporator\n"
+            "D. Condenser"
+        ),
+        "answer": (
+            "D. Condenser"
+        ),
+    },
+    {
+        "question": ( #043
+            "Where is the evaporator located?\n\n"
+            "A. Somewhere outside of the vehicle\n"
+            "B. Behind the firewall in the passenger compartment\n"
+            "C. In front of the firewall in the engine compartment"
+        ),
+        "answer": (
+            "B. Behind the firewall in the passenger compartment"
+        ),
+    },
+    {
+        "question": ( #044
+            "A car has an internal combustion engine.\n"
+            "How is the compressor driven?\n\n"
+            "A. By the internal combustion engine\n"
+            "B. By an electric motor\n"
+            "C. By the surrounding ambient air"
+        ),
+        "answer": (
+            "A. By the internal combustion engine"
+        ),
+    },
+    {
+        "question": ( #045
+            "How are AC components linked together?\n\n"
+            "A. With coolant hoses\n"
+            "B. With electric wire\n"
+            "C. With aluminum lines and and special high-pressure hoses"
+        ),
+        "answer": (
+            "C. With aluminum lines and special high-pressure hoses"
+        ),
+    },
+    {
+        "question": ( #046
+            "The refrigerant needs to enter the evaporator in the right state, at the right temperature, and at the right pressure.\n"
+            "Why is this important?\n"
+            "A. Otherwise the refrigerant cannot condense\n"
+            "B. Otherwise the refrigerant cannot evaporate\n"
+            "C. Otherwise the refrigerant cannot change from gas to liquid"
+        ),
+        "answer": (
+            "B. Otherwise the refrigerant cannot evaporate"
+        ),
+    },
+    {
+        "question": ( #047
+            "What happens when refrigerant flows through the condenser\n\n"
+            "A. The refrigerant pressure rises, and it changes from a liquid to a gas\n"
+            "B. The refrigerant changes from a gas to a liquid\n"
+            "C. The refrigerant temperature rises, and it changes from a gas to a liquid"
+        ),
+        "answer": (
+            "B. The refrigerant changes from a gas to a liquid"
+        ),
+    },
+]
