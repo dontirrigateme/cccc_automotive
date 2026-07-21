259a203fddca584bbfbab9f76f5f23286b65e3cc
dontirrigateme <smokey.alley44@gmail.com>
2026-07-21 01:22:41 -0400

    Add engine performance systems quiz questions

---
+++ study_guide_data/engine_perf_sys.py
@@ -0,0 +1,292 @@
+# AUT-181 Engine Performance 1 - ENGINE PERFORMANCE SYSTEMS QUIZ
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "The PCM can be electronically linked to other control modules.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #002
+            "OBD-II regulations require all vehicle manufacturers to use the same names and acronyms for all electric and electronic systems related to the engine and emissions control systems.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #003
+            "You can use a generic scan tool to access a manufacturer PID.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False"
+        ),
+    },
+    {
+        "question": ( #004
+            "The MIL light should illuminate when the ignition switch is turned to the run position (engine not running) and go out within a few seconds after the engine is started.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #005
+            "Scan tools can be used to cycle on and off some actuators, such as injectors, vacuum solenoids, idle air control motors, and the like.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #006
+            "In an OBD-II system, each component is checked by a diagnostic routine to verify that it is functioning properly.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #007
+            "Which of the following jobs is NOT a function of the ignition system?\n\n"
+            "A. Generting a spark with enough heat to ignite the fuel\n"
+            "B. Engaging the starter motor\n"
+            "C. Delivering the spark at the proper time to each cylinder on the compression stroke\n"
+            "D. Maintaining the spark long enough for total combustion of air and fuel"
+        ),
+        "answer": (
+            "B. Engaging the starter motor"
+        ),
+    },
+    {
+        "question": ( #008
+            "Engine vacuum is being discussed. Technician A says that when the engine is operating under light loads, engine vacuum is low. Technician B says that engine vacuum is high when the engine is operated under heavy loads. Who is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "D. Neither technician A nor technician B"
+        ),
+    },
+    {
+        "question": ( #009
+            "Ignition spark typically occurs a few degrees __________.\n\n"
+            "A. Before bottom dead center (BBDC)\n"
+            "B. Before top dead center (BTDC)\n"
+            "C. After bottom dead center (ABDC)\n"
+            "D. After top dead center (ATDC)"
+        ),
+        "answer": (
+            "B. Before top dead center (BTDC)"
+        ),
+    },
+    {
+        "question": ( #010
+            "Which of the following is NOT an advantage of an electronic spark control system?\n\n"
+            "A. It makes changes many times faster than older (mechanical) systems\n"
+            "B. It allows lower compression ratio engines to be as efficient as higher compression ratio engines\n"
+            "C. It compensates for changes in engine (and sometimes outside air) temperature\n"
+            "D. It has a feedback mechanism which allows it to constantly compensate for changing conditions"
+        ),
+        "answer": (
+            "B. It allows lower compression ratio engines to be as efficient as higher compression ration engines"
+        ),
+    },
+    {
+        "question": ( #011
+            "In a gasoline direct injection (GDI) system, the injector sprays fuel into the __________.\n\n"
+            "A. Cylinders (combustion chamber)\n"
+            "B. Intake runner in manifold\n"
+            "C. Intake port in cylinder head\n"
+            "D. Intake manifold (plenum)"
+        ),
+        "answer": (
+            "A. Cylinders (combustion chamber)"
+        ),
+    },
+    {
+        "question": ( #012
+            "The injector pulse width is the __________.\n\n"
+            "A. Size of the injector nozzle\n"
+            "B. Distance between the injector tip and the center of the intake port\n"
+            "C. Voltage applies to the injector solenoid coil\n"
+            "D. Amount of time the injector opens and sprays fuel"
+        ),
+        "answer": (
+            "D. Amount of time the injector opens and sprays fuel"
+        ),
+    },
+    {
+        "question": ( #013
+            "Engine operating modes are being discussed. Technician A says that when the engine is in closed-loop mode, the PCM monitors sensor inputs, then adjusts the timing, air fuel ratio, and emission controls. Technician B says the system will switch into open-loop when the throttle is fully opened or the oxygen sensors cool down during idle. Who is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "C. Both technician A and technician B"
+        ),
+    },
+    {
+        "question": ( #014
+            "Catalytic converter monitors are being discussed. Technician A says that if the converter is working, the downstream signal frequency should mirror the upstream sensor. Technician B says that the downstream sensor should provide a low-frequency voltage signal. Who is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "B. Technician B"
+        ),
+    },
+    {
+        "question": ( #015
+            "What sensor is used to detect engine misfire?\n\n"
+            "A. Cylinder-head temperature\n"
+            "B. Crankshaft position\n"
+            "C. Knock\n"
+            "D. Exhaust-gas temperature"
+        ),
+        "answer": (
+            "B. Crankshaft position"
+        ),
+    },
+    {
+        "question": ( #016
+            "Which of the following statements is NOT true?\n\n"
+            "A. A type-B misfire is set after detected faults in two consecutive drive cycles\n"
+            "B. A type-B misfire will cause the MIL light to flash\n"
+            "C. A type-A misfire will set a DTC and cause the MIL light to illuminate immediately\n"
+            "D. A type-A misfire can cause damage to the catalytic converter if the injector is not shut down"
+        ),
+        "answer": (
+            "B. A type-B misfire will cause the MIL light to flash"
+        ),
+    },
+    {
+        "question": ( #017
+            "Oxygen sensor monitors are being discussed. Technician A says the system monitors the sensor response time as the system cycles lean-rich-lean. Technician B says that at certain times the monitor varies the fuel delivery and checks for heated oxygen sensor response. Who is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "C. Both technician A and technician B"
+        ),
+    },
+    {
+        "question": ( #018
+            "Diagnostic trouble codes are being discussed. Technician A says a P0442 is a manufacturer’s code and means different things, depending on what manufacturer built the car. Technician B says that a P0304 code is a universal code that means the same thing no matter what manufacturer built the car. Who is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "B. Technician B"
+        ),
+    },
+    {
+        "question": ( #019
+            "Freeze-frame data is being discussed. Technician A says erasing the code with a scan tool will erase the corresponding freeze-frame data. Technician B says the freeze-frame data is the sensor and output status data at the instant the DTC was set. Who is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "C. Both technician A and technician B"
+        ),
+    },
+    {
+        "question": ( #020
+            "Before performing any repairs on a vehicle with an illuminated MIL light: Technician A records all DTCs and freeze-frame data. Technician B checks for service bulletins.Who is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "C. Both technician A and technician B"
+        ),
+    },
+    {
+        "question": ( #021
+            "Technician A uses the snapshot function of his scan tool to record serial data during a road test that can be transferred and displayed on a PC. Technician B uses the snapshot feature of his scan tool to record the sensor data and conditions leading up to the vehicle setting a DTC. Who is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "A. Technician A"
+        ),
+    },
+    {
+        "question": ( #022
+            "Which of the following sensors is NOT used by the computerized ignition system to control ignition timing?\n\n"
+            "A. Oxygen sensor\n"
+            "B. Manifold vacuum\n"
+            "C. Engine temperature\n"
+            "D. Engine RPM"
+        ),
+        "answer": (
+            "A. Oxygen sensor"
+        ),
+    },
+    {
+        "question": ( #023
+            "The two types of ignition systems are electronic ignition (EI) and __________.\n\n"
+            "A. Distributor ignition (DI)\n"
+            "B. Ground-circuit ignition (GCI)\n"
+            "C. Magneto ignition (MI)\n"
+            "D. Multispark ignition (MSI)"
+        ),
+        "answer": (
+            "A. Distributor ignition (DI)"
+        ),
+    },
+    {
+        "question": ( #024
+            "The DLC connector on an OBD-II vehicle is located under the __________.\n\n"
+            "A. Dash on the driver\'s side\n"
+            "B. Hood, near the battery\n"
+            "C. Hood, on the firewall\n"
+            "D. Hood, in the fuse/relay center"
+        ),
+        "answer": (
+            "A. Dash on the driver\'s side"
+        ),
+    },
+    {
+        "question": ( #025
+            "OBD II technology is being discussed. Technician A says the OBD II system has the ability to store a freeze frame (snapshot) of the operating conditions that existed when the fault occurred. Technician B says vehicle identification must be transmitted automatically to the scan tool. Who is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "C. Both technician A and technician B"
+        ),
+    },
+]
