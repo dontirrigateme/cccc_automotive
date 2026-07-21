51259b0d518cdd1a7e13e5c3440fb260eca6a89d
dontirrigateme <smokey.alley44@gmail.com>
2026-07-21 00:09:59 -0400

    Add AC quiz questions and answers to ac_quiz_avi.py

---
+++ study_guide_data/ac_quiz_avi.py
@@ -0,0 +1,62 @@
+# AVI - KEEPING IT COOL - AC SYSTEMS
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "After repairs, an AC system should be evacuated for:\n\n"
+            "A. 15 minutes\n"
+            "B. 45 minutes\n"
+            "C. 1 hour\n"
+            "D. 2 hours"
+        ),
+        "answer": (
+            "B. 45 minutes"
+        ),
+    },
+    {
+        "question": ( #002
+            "When replacing a compressor, the following parts must be replaced:\n\n"
+            "A. Orifice tube\n"
+            "B. Condenser\n"
+            "C. Receiver drier\n"
+            "D. All of the above"
+        ),
+        "answer": (
+            "D. All of the above"
+        ),
+    },
+    {
+        "question": ( #003
+            "Liquid refrigerant should absorb heat in the evaporator.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False"
+        ),
+    },
+    {
+        "question": ( #004
+            "How many ounces of refrigerant can your AC service hoses hold?\n\n"
+            "A. 2 oz\n"
+            "B. 3 oz\n"
+            "C. 4 oz\n"
+            "D. 6 oz"
+        ),
+        "answer": (
+            "D. 6 oz"
+        ),
+    },
+    {
+        "question": ( #005
+            "When the ambient temperature is too low for AC, the electronic variable compressor:\n\n"
+            "A. Reduces displacement\n"
+            "B. Increases displacement\n"
+            "C. Enters a defrost cycle\n"
+            "D. Stops spinning"
+        ),
+        "answer": (
+            "A. Reduces displacement"
+        ),
+    },
+]
