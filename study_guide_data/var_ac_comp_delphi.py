d2fd50e9b5fb89adba77b341a5b620fbdfddb951
dontirrigateme <smokey.alley44@gmail.com>
2026-07-29 14:03:08 -0400

    Add HVAC diagnostic questions for study guide
    Added a series of questions and answers related to HVAC systems, focusing on variable displacement compressors and diagnostic processes.

---
+++ study_guide_data/var_ac_comp_delphi.py
@@ -0,0 +1,118 @@
+# AVI - KEEPING IT COOL - AC SYSTEMS
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "Two technicians are discussing a list of HVAC module DTCs. Technician A says that B-Codes related to temperature sensor problems should be addressed first in the diagnostic process. Technician B says that U-codes should be addressed before looking at B-codes. Which technician is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "B. Technician B"
+        ),
+    },
+    {
+        "question": ( #002
+            "Data PIDs for variable displacement compressors on various OEMs may show any of the following EXCEPT:\n\n"
+            "A. Solenoid valve electrical status for shorted or open\n"
+            "B. Solenoid valve command current draw\n"
+            "C. Solenoid valve commanded frequency\n"
+            "D. Solenoid valve command percentage"
+        ),
+        "answer": (
+            "C. Solenoid valve commanded frequency"
+        ),
+    },
+    {
+        "question": ( #003
+            "All variable displacement compressors use electric solenoids to control high side pressure.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False"
+        ),
+    },
+    {
+        "question": ( #004
+            "Two technicians are discussing a vehicle with a considerably lower A/C high pressure reading on the scan tool compared to the actual high-pressure gauge reading. Technician A says that the problem could a faulty high side A/C pressure sensor. Technician B says the problem could be due to a restriction in the condenser liquid line. Which technician is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "C. Both technician A and technician B"
+        ),
+    },
+    {
+        "question": ( #005
+            "Two technicians are discussing variable displacement compressors. Technician A says if the compressor does NOT have a clutch, it uses a solenoid for displacement control. Technician B says some OEMs give no means via their factory scan tool to control the variable displacement solenoid valve duty cycle. Which technician is correct?\n\n"
+            "A. Technician A\n"
+            "B. Technician B\n"
+            "C. Both technician A and technician B\n"
+            "D. Neither technician A nor technician B"
+        ),
+        "answer": (
+            "C. Both technician A and technician B"
+        ),
+    },
+    {
+        "question": ( #006
+            "An IHX (Internal Heat Exchanger) assists with A/C system:\n\n"
+            "A. Superheating\n"
+            "B. Subcooling"
+        ),
+        "answer": (
+            "B. Subcooling"
+        ),
+    },
+    {
+        "question": ( #007
+            "Pressure style variable displacement control valves and solenoid style variable displacement control valves can be replaced on some vehicles while the compressor is still mounted on the vehicle.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #008
+            "A vehicle with a variable-displacement compressor is not cooling. A solenoid-activation tool is connected to change the compressor displacement and the A/C then begins to cool What is the most likely cause?\n\n"
+            "A. An open circuit on the variable-displacement solenoid wire from the ECU\n"
+            "B. A system extremely low or empty on refrigerant\n"
+            "C. An open winding within the variable displacement compressor\’s control solenoid\n"
+            "D. System refrigerant overcharge"
+        ),
+        "answer": (
+            "A. An open circuit on the variable-displacement solenoid wire from the ECU"
+        ),
+    },
+    {
+        "question": ( #009
+            "A late model GM vehicle with a variable-displacement compressor has an open in the circuit to the compressor’s pressure-control solenoid. Which module would most likely set a DTC?\n\n"
+            "A. HVAC\n"
+            "B. IPC\n"
+            "C. PCM\n"
+            "D. BCM"
+        ),
+        "answer": (
+            "A. HVAC"
+        ),
+    },
+    {
+        "question": ( #010
+            "A variable displacement compressor without a mechanical clutch:\n\n"
+            "A. Uses break-away shear points to allow a frozen compressor to lock without breaking the drive belt\n"
+            "B. Can receive a 0 percent duty cycle in order to get little to no compressor piston movement\n"
+            "C. Both\n"
+            "D. Neither"
+        ),
+        "answer": (
+            "C. Both"
+        ),
+    },
+]
