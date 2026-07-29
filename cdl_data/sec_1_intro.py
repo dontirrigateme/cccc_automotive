19defff3b634d53c41ed85dd982d6f2614002536
dontirrigateme <smokey.alley44@gmail.com>
2026-07-29 13:06:14 -0400

    Add introduction section to sec_1_intro.py

---
+++ cdl_data/sec_1_intro.py
@@ -0,0 +1,16 @@
+# Section 1 - Introduction
+
+# QUESTIONS = [
+#     {
+#         "question": ( #001
+#             "\n\n"
+#             "A. \n"
+#             "B. \n"
+#             "C. \n"
+#             "D. "
+#         ),
+#         "answer": (
+#             ""
+#         ),
+#     },
+# ]
