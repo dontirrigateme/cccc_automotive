8d4c8e36098f9736146d69008cf48c8dcf9b84be
dontirrigateme <smokey.alley44@gmail.com>
2026-07-29 15:32:35 -0400

    Add AC compressor questions and answers
    Added a comprehensive set of questions and answers regarding air-conditioning compressors, covering various aspects of their operation and types.

---
+++ electude_data/ac_comp.py
@@ -0,0 +1,377 @@
+# ELECTUDE - AIR-CONDITIONING COMPRESSORS
+
+QUESTIONS = [
+    {
+        "question": ( #001
+            "How is the compressor of an AC system driven in a vehicle with an internal combustion engine?\n\n"
+            "A. By an electric motor\n"
+            "B. By the internal combustion engine\n"
+            "C. By the driveline"
+        ),
+        "answer": (
+            "B. By the internal combustion engine\n"
+            "The internal combustion engine drives the air-conditioning compressor via a belt. In hybrid and electric vehicles, the compressor is driven by an electric motor."
+        ),
+    },
+    {
+        "question": ( #002
+            "A compressor changes refrigerant from gas to liquid.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False"
+        ),
+    },
+    {
+        "question": ( #003
+            "A compressor changes the pressure of refrigerant from low to high.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #004
+            "A compressor changes the temperature of refrigerant from low to high.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #005
+            "Liquid cannot be compressed\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #006
+            "In what state must the refrigerant be when it enters an AC compressor?\n\n"
+            "A. High-pressure gas\n"
+            "B. High-pressure liquid\n"
+            "C. Low-pressure liquid\n"
+            "D. Low-pressure gas"
+        ),
+        "answer": (
+            "D. Low-pressure gas"
+        ),
+    },
+    {
+        "question": ( #007
+            "How does a vane compressor draw in refrigerant?\n\n"
+            "A. By increasing the volume of the chamber at the at the inlet line\n"
+            "B. By increasing the volume of the chamber at the high-pressure outlet line\n"
+            "C. By connecting the high-pressure AC line to the inlet line"
+        ),
+        "answer": (
+            "A. By increasing the volume of the chamber at the inlet line\n"
+            "Increasing the volume creates a vacuum, drawing refrigerant into the compressor."
+        ),
+    },
+    {
+        "question": ( #008
+            "How does a vane compressor build up pressure?\n\n"
+            "A. By reducing the volume of the chamber at the high-pressure line\n"
+            "B. By making the refrigerant flow faster\n"
+            "C. By increasing the volume of the chamber at the inlet line"
+        ),
+        "answer": (
+            "A. By reducing the voolume of the chamber at the high-pressure line\n"
+            "By reducing the volume of the chamber, the pressure increases, and the refrigerant leaves the compressor pressurized."
+        ),
+    },
+    {
+        "question": ( #009
+            "What kind of motion does the moving scroll in a scroll-type compressor make?\n\n"
+            "A. An eccentric rotation\n"
+            "B. An oscillating rotation\n"
+            "C. A centric rotation"
+        ),
+        "answer": (
+            "A. An eccentric rotation\n"
+            "The scroll makes a circular motion."
+        ),
+    },
+    {
+        "question": ( #010
+            "How does a scroll compressor draw in refrigerant?\n\n"
+            "A. The scrolls rotate, and refrigerant flows to the high-pressure line\n"
+            "B. The scrolls move towards each other, and the volume available for the refrigerant decreases\n"
+            "C. The scrolls move away from each other, and the volume available for the refrigerant increases"
+        ),
+        "answer": (
+            "C. The scrolls move away from each other, and the volume available for the refrigerant increases\n"
+            "The scrolls move away from one another; as a result, the volume increases and the pressure drops."
+        ),
+    },
+    {
+        "question": ( #011
+            "How does the scroll compressor force out the refrigerant?\n\n"
+            "A. The scrolls move towards each other, and the volume available for the refrigerant decreases\n"
+            "B. The scrolls turn around, and the refrigerant flows to the inlet pipe\n"
+            "C. The scrolls move away from each other, and the volume available for the refrigerant increases"
+        ),
+        "answer": (
+            "A. The scrolls move towards each other, and the volume available for the refrigerant decreases\n"
+            "The moving scroll forces the refrigerant to the center of the compressor, where the refrigerant reaches the pressure line."
+        ),
+    },
+    {
+        "question": ( #012
+            "How can the output of a scroll compressor be affected?\n\n"
+            "A. By increasing the inlet pressure of the compressor\n"
+            "B. By fitting bigger AC lines to the compressor\n"
+            "C. By varying the rotational speed"
+        ),
+        "answer": (
+            "C. By varying the rotational speed\n"
+            "The output of the scroll compressor can be regulated by the speed. This is possible if the scroll compressor is electrically driven."
+        ),
+    },
+    {
+        "question": ( #013
+            "A swashplate compressor may have up to 10 pistons.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False\n"
+            "A swashplate compressor may have up to 6 pistons."
+        ),
+    },
+    {
+        "question": ( #014
+            "How does the swashplate compressor draw in refrigerant?\n\n"
+            "A. By rotating the pistons through the compressor\n"
+            "B. By enlarging the volume above the piston\n"
+            "C. By reducing the volume above the piston"
+        ),
+        "answer": (
+            "B. By enlarging the volume above the piston\n"
+            "Enlarging the chamber above the pistons generates a vacuum, causing refrigerant to flow in."
+        ),
+    },
+    {
+        "question": ( #015
+            "What happens to the variable swashplate compressor when the outside temperature is very low?\n\n"
+            "A. The compressor refrigerant output decreases\n"
+            "B. The compressor refrigerant output stays the same\n"
+            "C. The compressor refrigerant output increases"
+        ),
+        "answer": (
+            "A. The compressor refrigerant output decreases\n"
+            "At a low temperature, less refrigerant flows through the system because the cooling demand is lower."
+        ),
+    },
+    {
+        "question": ( #016
+            "How does a sensor's control unit protect the air-conditioning system?\n\n"
+            "A. By opening the compressor pressure-relief valve\n"
+            "B. By switching on the compressor\n"
+            "C. By switching off the compressor\n"
+            "D. By closing the compressor pressure-relief valve"
+        ),
+        "answer": (
+            "C. By switching off the compressor"
+        ),
+    },
+    {
+        "question": ( #017
+            "What happens if the pressure in an air-conditioning system is too low?\n\n"
+            "A. The control unit switches off the air-conditioning compressor\n"
+            "B. The control unit allows the air-conditioning system to run faster\n"
+            "C. The pressure in the air-conditioning system continues to rise"
+        ),
+        "answer": (
+            "A. The control unit switches off the air-conditioning compressor\n"
+            "The control unit receives a signal indicating that the pressure is too low. The compressor is switched off."
+        ),
+    },
+    {
+        "question": ( #018
+            "What happens if the pressure in an air-conditioning system is too high?\n\n"
+            "A. The pressure in the air-conditioning system continues to rise\n"
+            "B. The control unit switches the compressor off\n"
+            "C. The control unit causes the compressor to run slower"
+        ),
+        "answer": (
+            "B. The control unit switches the compressor off\n"
+            "The control unit receives a signal indicating that the pressure is too high. The control unit responds by switching the compressor off."
+        ),
+    },
+    {
+        "question": ( #019
+            "Why does the control unit of an air-conditioning system switch the compressor off if the evaporator temperature is too low?\n\n"
+            "A. The low temperature results in a high cooling capacity of the air-conditioning system\n"
+            "B. The low temperature results in ice forming over the evaporator\n"
+            "C. The low temperature ensures complete evaporation of the refrigerant in the evaporator"
+        ),
+        "answer": (
+            "B. The low temperature results in ice forming over the evaporator"
+        ),
+    },
+    {
+        "question": ( #020
+            "What is the result of ice formation over the evaporator?\n\n"
+            "A. The refrigerant flow through the evaporator stops\n"
+            "B. The air flow through the evaporator cools off too much\n"
+            "C. The heat exchange between the evaporator and the ambient air decreases"
+        ),
+        "answer": (
+            "C. The heat exchange between the evaporator and the ambient air decreases"
+        ),
+    },
+    {
+        "question": ( #021
+            "What is the risk if there is poor heat exchange between the evapoprator and the ambient air?\n\n"
+            "A. Liquid refrigerant could leave the evaporator and damage the compressor\n"
+            "B. Refrigerant freezes as it flows through the evaporator\n"
+            "C. The cooling capacity increases and cools the air flow too much"
+        ),
+        "answer": (
+            "A. Liquid refrigerant could leave the evapoprator and damage the compressor\n"
+            "The air-conditioning compressor cannot compress liquid refrigerant."
+        ),
+    },
+    {
+        "question": ( #022
+            "What is the name of the part that ensures that the air-conditioning compressor can be switched on and off?\n\n"
+            "A. The magnetic clutch\n"
+            "B. The compressor housing\n"
+            "C. The serpentine belt"
+        ),
+        "answer": (
+            "A. The magnetic clutch"
+        ),
+    },
+    {
+        "question": ( #023
+            "How is the drive for the air-conditioning compressor interrupted?\n\n"
+            "A. The control unit deactivates the magnetic clutch\n"
+            "B. The control unit activates the magnetic clutch"
+        ),
+        "answer": (
+            "A. The control unit deactivates the magnetic clutch\n"
+            "If the clutch is deactivated, the drive for the air-conditioning compressor is disabled."
+        ),
+    },
+    {
+        "question": ( #024
+            "Vane-type and scroll-type air-conditioning compressors are variable-displacement compressors.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False\n"
+            "Vane-type and scroll-type compressors are fixed-displacement compressors."
+        ),
+    },
+    {
+        "question": ( #025
+            "In a vane-type air-conditioning compressor, what happens to the size of the chambers as the vanes spin?\n\n"
+            "A. The size of the chambers change\n"
+            "B. The size of the chambers stay the same\n"
+            "C. There aren't chambers in a vane-type air-conditioning compressor"
+        ),
+        "answer": (
+            "A. The size of the chambers change"
+        ),
+    },
+    {
+        "question": ( #026
+            "The scroll compressor consists of a casing that contains a fixed (static) scroll, also referred to as a helix, and a driven scroll in the open area of the fixed scroll.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #027
+            "The swashplate compressor is a piston-type compressor.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #028
+            "There are two types of swashplate air-conditioning compressors: the fixed-displacement swashplate compressor and variable swashplate compressor.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "A. True"
+        ),
+    },
+    {
+        "question": ( #029
+            "In a variable swashplate compressor, the displacement of the compressor is determined by the speed of the pistons.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False\n"
+            "The displacement of a variable swashplate compressor is determined by the angle of the swashplate."
+        ),
+    },
+    {
+        "question": ( #030
+            "In an air-conditioning system, if the high-pressure relief valve is not located on the compressor, where is the most likely place to find it?\n\n"
+            "A. Accumulator\n"
+            "B. Evaporator\n"
+            "C. Condenser\n"
+            "D. Receiver drier"
+        ),
+        "answer": (
+            "D. Receiver drier"
+        ),
+    },
+    {
+        "question": ( #031
+            "Why can a thermal switch be used to protect an air-conditioning system from high pressure?\n\n"
+            "A. It reads the temperature of the air blowing out of the vents\n"
+            "B. It allows the compressor to be turned on or off manually\n"
+            "C. If the pressure in the compressor increases, the temperature will increase as well\n"
+            "D. A thermal switch cannot be used to protect from high pressure"
+        ),
+        "answer": (
+            "C. If the pressure in the compressor increases, the temperature will increase as well"
+        ),
+    },
+    {
+        "question": ( #032
+            "The air-conditioning compressor of an EV is equipped with:\n\n"
+            "A. Blue wires\n"
+            "B. Red and black wires\n"
+            "C. Orange high-voltage cabling\n"
+            "D. A serpentine belt"
+        ),
+        "answer": (
+            "C. Orange high-voltage cabling"
+        ),
+    },
+    {
+        "question": ( #033
+            "The electric motor of an air-conditioning compressor is a 5-phase motor with permanent magnets.\n\n"
+            "A. True\n"
+            "B. False"
+        ),
+        "answer": (
+            "B. False\n"
+            "The electric motor of an air-conditioning compressor is a 3-phase motor with permanent magnets."
+        ),
+    },
+]
