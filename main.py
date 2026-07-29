8ad13ef8730b45fee93a24b6e19860172922c83f
dontirrigateme <smokey.alley44@gmail.com>
2026-07-29 16:19:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ from study_guide_data.ac_quiz_avi import QUESTIONS as ac_quiz_avi_questions
 from study_guide_data.elec_fuel_inj import QUESTIONS as elec_fuel_inj_questions
 from study_guide_data.engine_perf_sys import QUESTIONS as engine_perf_sys_questions
 from study_guide_data.var_ac_comp_delphi import QUESTIONS as var_ac_comp_delphi_questions
-from study_guide_data.brakes_quiz import QUESTIONS as brake_quiz_questions
+from study_guide_data.brakes_quiz import QUESTIONS as brakes_quiz_questions
 
 # from cdl_data.sec_1_intro import QUESTIONS as sec_1_intro_questions
 
